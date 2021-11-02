596cf25f8c12c51ad3830f154a14fbe8ddf3a783
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2021-11-02 12:29:55 +0300

    PLATFORM01-8369: nx implement theme

--- libs/components/.storybook/preview.js
+++ libs/components/.storybook/preview.js
@@ -2,10 +2,4 @@
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
 
-// import "!style-loader!css-loader!../styles/primeng.css";
-// import "!style-loader!css-loader!../styles/themes/luna-blue/theme.css";
-// import "!style-loader!css-loader!../styles/primeicons/primeicons.css";
-// import "!style-loader!css-loader!less-loader!../node_modules/zyfra-theme/lib/default.css";
-// import "!style-loader!css-loader!less-loader!../node_modules/zyfra-theme/lib/styles.less";
-
 setCompodocJson(docJson);
--- libs/components/src/lib/button/zyfra-button.component.stories.ts
+++ libs/components/src/lib/button/zyfra-button.component.stories.ts
@@ -5,7 +5,7 @@ import { ZyfraButtonComponent } from './zyfra-button.component';
 import doc from './zyfra-button.component.story.doc.mdx';
 
 export default {
-  title: 'ZyfraButtonComponent',
+  title: 'Buttons/Button',
   component: ZyfraButtonComponent,
   decorators: [
     moduleMetadata({
@@ -25,14 +25,67 @@ const Template: Story<ZyfraButtonComponent> = (args: ZyfraButtonComponent) => ({
 });
 
 
-export const Primary = Template.bind({});
-Primary.args = {
-    label:  '',
-    type:  'button',
-    icon:  '',
-    iconPos:  'left',
-    disabled:  false,
-    badge:  '',
-    style:  '',
-    styleClass:  '',
-}
+export const Simple = Template.bind({});
+Simple.args = {
+  label: 'Button',
+  type:'button',
+  styleClass: 'btn-default btn-primary'
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  label: 'disabled Button',
+  styleClass: 'btn-secondary'
+};
+
+
+export const Warning = Template.bind({});
+Warning.args = {
+  disabled: false,
+  label: 'Warning',
+  styleClass: 'p-button-warning'
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+  disabled: false,
+  label: 'Delete',
+  color:  'secondary',
+  styleClass: 'p-button-danger'
+};
+
+
+
+export const Rounded = Template.bind({});
+Rounded.args = {
+  label: 'Rounded',
+  styleClass: 'p-button-rounded'
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  icon:"pi pi-check",
+  iconPos:"left",
+  label: 'Icon',
+};
+
+export const OnlyIcon = Template.bind({});
+OnlyIcon.args = {
+  icon:"pi pi-check",
+};
+
+export const Submit = Template.bind({});
+Submit.args = {
+  label:"Submit",
+  type:"submit",
+  icon:"pi pi-check",
+  iconPos:"right",
+};
+
+export const Badge = Template.bind({});
+Badge.args = {
+  label:"with Badge 8",
+  badge: "8",
+};
+
