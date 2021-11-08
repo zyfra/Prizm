5c864a6d895ce30371a8c366d655923b07a20a0d
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2021-11-08 17:35:10 +0300

    fix: typo

--- libs/schematics/src/ng-add/setup/index.ts
+++ libs/schematics/src/ng-add/setup/index.ts
@@ -1,11 +1,8 @@
-import {chain, Rule} from '@angular-devkit/schematics';
-import { Schema } from "../schema";
+import { chain, Rule } from '@angular-devkit/schematics';
+import { Schema } from '../schema';
 import { addInstallMainModules } from './modules';
 import { addStyles } from './styles';
 
 export default function ngAddSetupProject(options: Schema): Rule {
-    return chain([
-        addStyles(options),
-        addInstallMainModules(options),
-    ]);
+  return chain([addStyles(options), addInstallMainModules(options)]);
 }
