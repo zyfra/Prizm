0a962c8f5d510426bb12c5aebea056dcab55c041
VakarchukD <ozk.nemoy@mail.ru>
2021-11-10 11:33:40 +0300

    update buttons component

--- libs/components/src/lib/button/zyfra-button.component.stories.ts
+++ libs/components/src/lib/button/zyfra-button.component.stories.ts
@@ -1,91 +1,117 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
-import { ZyfraButtonModule } from '@ui-platform/ui-components';
+import { CommonModule } from '@angular/common';
+import { moduleMetadata } from '@storybook/angular';
+import { Story, Meta } from '@storybook/angular/types-6-0';
+import { BrowserModule } from '@angular/platform-browser';
+// @ts-ignore
+import ButtonDoc from './zyfra-button.component.story.doc.mdx';
 import { ZyfraButtonComponent } from './zyfra-button.component';
-//@ts-ignore
-import doc from './zyfra-button.component.story.doc.mdx';
+import { ZyfraButtonModule } from './zyfra-button.module';
 
 export default {
+  moduleId: module.id,
   title: 'Buttons/Button',
   component: ZyfraButtonComponent,
   decorators: [
     moduleMetadata({
-      imports: [ZyfraButtonModule],
-    })
+      imports: [BrowserModule, CommonModule, ZyfraButtonModule],
+    }),
   ],
   parameters: {
     docs: {
-      page: doc
-    }
-  }
-} as Meta<ZyfraButtonComponent>;
+      page: ButtonDoc,
+    },
+  },
+} as Meta;
 
-const Template: Story<ZyfraButtonComponent> = (args: ZyfraButtonComponent) => ({
+const Template: Story<ZyfraButtonComponent> = (args) => ({
   component: ZyfraButtonComponent,
   props: args,
 });
 
-
-export const Simple = Template.bind({});
-Simple.args = {
-  label: 'Button',
-  type:'button',
-  styleClass: 'btn-default btn-primary'
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Default',
+  type: 'button',
+  styleClass: '',
 };
 
-export const Disabled = Template.bind({});
-Disabled.args = {
+export const DefaultMini = Template.bind({});
+DefaultMini.args = {
+  label: 'Default',
+  type: 'button',
+  styleClass: 'p-button-mini',
+};
+
+export const DefaultDisabled = Template.bind({});
+DefaultDisabled.args = {
   disabled: true,
   label: 'disabled Button',
-  styleClass: 'btn-secondary'
 };
 
+export const Success = Template.bind({});
+Success.args = {
+  label: 'Button',
+  type: 'button',
+  styleClass: 'p-button-success',
+};
+
+export const SuccessMini = Template.bind({});
+SuccessMini.args = {
+  label: 'Button',
+  type: 'button',
+  styleClass: 'p-button-success p-button-mini',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  disabled: false,
+  label: 'Delete',
+  styleClass: 'p-button-secondary',
+};
+
+export const Info = Template.bind({});
+Info.args = {
+  label: 'Info',
+  type: 'button',
+  styleClass: 'p-button-info',
+};
 
 export const Warning = Template.bind({});
 Warning.args = {
   disabled: false,
   label: 'Warning',
-  styleClass: 'p-button-warning'
+  styleClass: 'p-button-warning',
 };
 
 export const Danger = Template.bind({});
 Danger.args = {
   disabled: false,
   label: 'Delete',
-  color:  'secondary',
-  styleClass: 'p-button-danger'
-};
-
-
-
-export const Rounded = Template.bind({});
-Rounded.args = {
-  label: 'Rounded',
-  styleClass: 'p-button-rounded'
+  styleClass: 'p-button-danger',
 };
 
 export const Icon = Template.bind({});
 Icon.args = {
-  icon:"pi pi-check",
-  iconPos:"left",
+  icon: 'zyfra-icon selection-check-simple',
+  iconPos: 'left',
   label: 'Icon',
 };
 
 export const OnlyIcon = Template.bind({});
 OnlyIcon.args = {
-  icon:"pi pi-check",
+  icon: 'zyfra-icon selection-check-simple',
 };
 
 export const Submit = Template.bind({});
 Submit.args = {
-  label:"Submit",
-  type:"submit",
-  icon:"pi pi-check",
-  iconPos:"right",
+  label: 'Submit',
+  type: 'submit',
+  icon: 'zyfra-icon selection-check-simple',
+  iconPos: 'right',
 };
 
 export const Badge = Template.bind({});
 Badge.args = {
-  label:"with Badge 8",
-  badge: "8",
+  label: 'with Badge 8',
+  badge: '8',
 };
-
