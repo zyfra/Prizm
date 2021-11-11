e53aab27f2acab52b4745956a67d05294109cb2f
VakarchukD <ozk.nemoy@mail.ru>
2021-11-11 17:40:49 +0300

    prettify textarea component

--- libs/components/src/lib/textarea/zyfra-textarea.module.ts
+++ libs/components/src/lib/textarea/zyfra-textarea.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'; 
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ZyfraTextareaComponent } from './zyfra-textarea.component';
-
-
 
 @NgModule({
   declarations: [ZyfraTextareaComponent],
-  imports: [
-    CommonModule,
-    FormsModule, 
-    ReactiveFormsModule,
-    InputTextareaModule
-  ],
-  exports: [ZyfraTextareaComponent]
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, InputTextareaModule],
+  exports: [ZyfraTextareaComponent],
 })
-export class ZyfraTextareaModule { }
+export class ZyfraTextareaModule {}
