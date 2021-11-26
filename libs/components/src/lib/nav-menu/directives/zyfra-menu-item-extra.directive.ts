eb715d460169baa915839f49b3acc97868f7e7b6
Anastasia Tsvirko <45286029+aFoxy@users.noreply.github.com>
2021-11-26 14:34:53 +0300

    remove unused directive

--- libs/components/src/lib/nav-menu/directives/zyfra-menu-item-extra.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive, TemplateRef } from '@angular/core';
-@Directive({
-  selector: '[zyfraMenuItemExtra]'
-})
-export class ZyfraMenuItemExtraDirective {
-  constructor(public template: TemplateRef<any>) {}
-}
