e2f47ced9c2b0b7873468360034c8cbd76164c09
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2021-11-26 18:52:27 +0300

    PLATFORM01-9306: Moved zyfra-datepicker with relative mode

---
+++ libs/components/src/lib/datepicker/zyfra-relative-datepicker/zyfra-relative-datepicker.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'zyfra-zyfra-relative-datepicker',
+  templateUrl: './zyfra-relative-datepicker.component.html',
+  styleUrls: ['./zyfra-relative-datepicker.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ZyfraRelativeDatepickerComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
