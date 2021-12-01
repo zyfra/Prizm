eb22834345d4641eedb279194d06c0a730d55cba
katkovergina <katkovergina@gmail.com>
2021-12-01 10:20:48 +0300

    datepicker: rangemode

--- libs/components/src/lib/datepicker/zyfra-datepicker.component.stories.ts
+++ libs/components/src/lib/datepicker/zyfra-datepicker.component.stories.ts
@@ -14,7 +14,7 @@ import DatepickerDoc from './zyfra-datepicker.component.story.doc.mdx';
 
 export default {
   moduleId: module.id,
-  title: 'Datapickers/Datapicker',
+  title: 'Datepickers/Datepicker',
   component: ZyfraDatepickerComponent,
   decorators: [
     moduleMetadata({
@@ -172,6 +172,36 @@ MultipleMode.args = {
   hideOnDateTimeSelect: false,
   numberOfMonths: 1,
   selectionMode: 'single',
+  disabledDates: null,
+  disabledDays: null,
+  firstDayOfWeek: 1,
+  locale: 'ru_RU',
+};
+
+
+export const RangeMode = Template.bind({});
+RangeMode.args = {
+  model: '21.09.2021 20:17:07',
+  dateFormat: 'DD.MM.YYYY',
+  returnFormatValue: 'string',
+  placeholder: 'Выберите дату и время',
+  disabled: false,
+  required: true,
+  label: 'Абсолютное',
+  spanClass: null,
+  timeMode: 'absolute',
+  showChangeMode: true,
+  showClear: true,
+  showDate: true,
+  showTime: false,
+  monthNavigator: false,
+  yearNavigator: false,
+  yearRange: null,
+  showCalendarBtn: true,
+  calendarBtnLabel: 'Выбрать',
+  hideOnDateTimeSelect: false,
+  numberOfMonths: 1,
+  selectionMode: 'range',
   disabledDates: null,
   disabledDays: null,
   firstDayOfWeek: 1,
