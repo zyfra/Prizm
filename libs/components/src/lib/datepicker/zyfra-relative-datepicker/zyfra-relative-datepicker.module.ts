47aa736d52d41bfad8c438693d079d5b3ef045df
RustamI <jamakrus@gmail.com>
2021-12-02 18:25:53 +0300

    PLATFORM01-9306: fixes datepicker

---
+++ libs/components/src/lib/datepicker/zyfra-relative-datepicker/zyfra-relative-datepicker.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ZyfraInputModule } from '@digital-plant/zyfra-components';
+import { ZyfraDropdownModule } from '../../dropdown';
+import { ZyfraRelativeDatepickerComponent } from './zyfra-relative-datepicker.component.js';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ZyfraInputModule,
+    ZyfraDropdownModule,
+    OverlayPanelModule
+  ],
+  declarations: [ZyfraRelativeDatepickerComponent],
+  exports: [ZyfraRelativeDatepickerComponent],
+})
+export class ZyfraRelativeDatepickerModule{}
