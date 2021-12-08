8f3d9fb85b6252d24da9e6f808fc856dc9816301
RustamI <jamakrus@gmail.com>
2021-12-08 14:52:36 +0300

    chore: up version to 0.0.6

--- libs/schematics/src/ng-add/consts.ts
+++ libs/schematics/src/ng-add/consts.ts
@@ -11,13 +11,13 @@ interface ImportingModule {
 export const MAIN_PACKAGES: ReadonlyArray<Package> = [
   {
     name: '@digital-plant/zyfra-components',
-    version: '0.0.5',
+    version: '0.0.6',
   },
 ];
 
 export const INSTALL_PACKAGE: Readonly<Package> = {
   name: '@digital-plant/zyfra-ui',
-  version: '0.0.5',
+  version: '0.0.6',
 };
 
 export const MAIN_MODULES: ReadonlyArray<ImportingModule> = [
