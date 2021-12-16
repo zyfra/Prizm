db04f75de88d913bbc0bbadda92c602330af3d78
Anton Osechkin <anton1.a@mail.ru>
2021-12-16 17:52:37 +0300

    Replace px with constants

--- libs/schematics/src/ng-add/setup/styles.ts
+++ libs/schematics/src/ng-add/setup/styles.ts
@@ -15,12 +15,11 @@ const INSTALL_STYLES = [
   'node_modules/primeng/resources/themes/luna-blue/theme.css',
   'node_modules/primeng/resources/primeng.min.css',
   'node_modules/primeicons/primeicons.css',
-  'node_modules/@digital-plant/zyfra-components/src/styles/theme/default.css',
+  'node_modules/@digital-plant/zyfra-components/src/styles/theme/default.less',
   'node_modules/@digital-plant/zyfra-components/src/styles/styles.less',
 ];
 
 const ICON_STYLES = ['node_modules/@digital-plant/zyfra-components/src/styles/icons/icons.less'];
-
 export function addStyles(options: Schema): Rule {
   return async (tree: Tree) => {
     const workspace = await getWorkspace(tree);
