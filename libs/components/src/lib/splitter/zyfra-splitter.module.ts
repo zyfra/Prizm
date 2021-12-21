8828f4f26cc3c54e47cde2f78bd6d1124e9518fd
stepandr <fstepan2010@gmail.com>
2021-12-21 13:59:58 +0000

    PLATFORM01-10021 fix nav menu

--- libs/components/src/lib/splitter/zyfra-splitter.module.ts
+++ libs/components/src/lib/splitter/zyfra-splitter.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { ZyfraSplitterComponent } from './zyfra-splitter.component';
 import { SplitterModule } from 'primeng/splitter';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { ZyfraSplitterTemplateDirective } from './zyfra-splitter.directives';
 
 @NgModule({
   declarations: [ZyfraSplitterTemplateDirective, ZyfraSplitterComponent],
-  imports: [CommonModule, BrowserModule, BrowserAnimationsModule, SplitterModule],
+  imports: [CommonModule, SplitterModule],
   exports: [ZyfraSplitterTemplateDirective, ZyfraSplitterComponent],
 })
 export class ZyfraSplitterModule {}
