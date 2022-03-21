bed95f940214a4bc9726bd4703b7ac4f3a98a1c1
Dmitrii Vakarchuk <dmitrii.vakarchuk@idp.zyfra.com>
2022-03-21 13:00:40 +0000

    PLATFORM01-9682 Dropdown VirtualScroll - некорректная работа компонента, страница тормозит

--- libs/components/src/lib/dropdown/zyfra-dropdown.component.stories.ts
+++ libs/components/src/lib/dropdown/zyfra-dropdown.component.stories.ts
@@ -88,7 +88,7 @@ Simple.args = {
   optionLabel: 'name',
   placeholder: 'Select a City',
   showClear: true,
-  label: 'City'
+  label: 'City',
 };
 
 export const Group = Template.bind({});
@@ -144,4 +144,11 @@ VirtualScroll.args = {
   virtualScroll: true,
   itemSize: 31,
   filter: false,
+};
+VirtualScroll.argTypes = {
+  options: {
+    control: {
+      disable: true,
+    },
+  },
 };
