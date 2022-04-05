b284740dee53656e90522cf2be15b32d8bc9bf02
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2022-04-05 13:26:25 +0000

    PLATFORM01-12991: Implement disable control directive

---
+++ libs/components/src/lib/@core/shared/zyfra-disable-control.module.ts
@@ -0,0 +1,20 @@
+import { Directive, Input, NgModule } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+@Directive({
+  selector: '[zyfraDisableControl]',
+})
+class ZyfraDisableControlDirective {
+  @Input() set zyfraDisableControl(condition: boolean) {
+    const action = condition ? 'disable' : 'enable';
+    this.ngControl.control[action]();
+  }
+
+  constructor(private ngControl: NgControl) {}
+}
+
+@NgModule({
+  declarations: [ZyfraDisableControlDirective],
+  exports: [ZyfraDisableControlDirective],
+})
+export class ZyfraDisableControlModule {}
--- libs/components/src/lib/input/zyfra-input.module.ts
+++ libs/components/src/lib/input/zyfra-input.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ZyfraDisableControlModule } from '../@core/shared/zyfra-disable-control.module';
 import { ZyfraInputComponent } from './zyfra-input.component';
 import { InputTextModule } from 'primeng/inputtext';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,6 +16,7 @@ import { ZyfraTooltipModule } from '../tooltip';
     FormsModule,
     ReactiveFormsModule,
     ZyfraTooltipModule,
+    ZyfraDisableControlModule
   ],
   exports: [ZyfraInputComponent],
 })
