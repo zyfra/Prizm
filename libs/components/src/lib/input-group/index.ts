ba808d2e812b38f3f9e217294991a7b48e61d069
RustamI <jamakrus@gmail.com>
2022-04-08 10:37:45 +0000

    Release 0.6.0

--- libs/components/src/lib/input-group/index.ts
+++ libs/components/src/lib/input-group/index.ts
@@ -1,3 +1,4 @@
 export * from './zyfra-input-group-control.class';
 export * from './zyfra-input-group.module';
 export * from './zyfra-input-group/zyfra-input-group.component';
+export * from './zyfra-input-group-clear/zyfra-input-group-clear.component';
