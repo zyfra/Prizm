a931465decefcaa7e51b534b5662cc1bd57cb72b
ZurabM <zurab@cnt.one>
2022-04-13 07:04:28 +0000

    Resolve PLATFORM01-13362 "Tooltip docs"

--- libs/components/src/lib/tooltip/zyfra-tooltip.component.stories.ts
+++ libs/components/src/lib/tooltip/zyfra-tooltip.component.stories.ts
@@ -1,18 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { moduleMetadata } from '@storybook/angular';
-import { Meta, Story } from '@storybook/angular/types-6-0';
+import { Meta, Story, moduleMetadata } from '@storybook/angular';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { ZyfraTooltipComponent } from './zyfra-tooltip.component';
+import { ZyfraTooltipDirective } from './zyfra-tooltip.directive';
 import { ZyfraTooltipModule } from './zyfra-tooltip.module';
 import { ZyfraButtonModule } from '../button';
 import { ZyfraTooltipOverlayManager } from './zyfra-tooltip-overlay-manager.service';
 
+
 export default {
   moduleId: module.id,
   title: 'Tooltip/Main',
-  component: ZyfraTooltipComponent,
+  component: ZyfraTooltipDirective,
   decorators: [
     moduleMetadata({
       imports: [
@@ -29,12 +29,42 @@ export default {
   ],
   parameters: {
     docs: {
-      page: require('./').default,
+      page: require('./zyfra-tooltip.component.story.doc.mdx').default,
+    },
+  },
+  args: {
+    delay: 400,
+    show: true,
+  },
+  argTypes: {
+    color:  {
+      control: {
+        type: "select",
+        options: [
+          'primary',
+          'secondary',
+          'success',
+          'info',
+          'warning',
+          'danger',
+        ]
+      }
+    },
+    position:  {
+      control: {
+        type: "select",
+        options: [
+          'left',
+          'right',
+          'above',
+          'below'
+        ]
+      }
     },
   },
 } as Meta;
 
-const Template: Story<ZyfraTooltipComponent> = (args) => ({
+const Template: Story<ZyfraTooltipDirective> = (args) => ({
   template: `
     <div
       style="height: 100vh;width: 100%;display: flex;justify-content: center;align-items: center;flex-direction: column;"
@@ -49,7 +79,9 @@ const Template: Story<ZyfraTooltipComponent> = (args) => ({
           <zyfra-button
             [ngStyle]="{display: 'block'}"
             [zyfraTooltip]="testTooltip"
+            [zyfraTooltipShow]="show"
             [zyfraTooltipColor]="color"
+            [zyfraTooltipDelay]="delay"
             [zyfraTooltipPosition]="position"
             label="Button"
             type="button"
@@ -63,6 +95,8 @@ const Template: Story<ZyfraTooltipComponent> = (args) => ({
             [ngStyle]="{display: 'block'}"
             [zyfraTooltip]="text"
             [zyfraTooltipColor]="color"
+            [zyfraTooltipShow]="show"
+            [zyfraTooltipDelay]="delay"
             [zyfraTooltipPosition]="position"
             label="Button"
             type="button"
@@ -81,7 +115,9 @@ TooltipWithTitle.args = {
     'В современных веб-интерфейсах граница между кнопками и ссылками размыта. Ссылка также может запускать действие. Отличие кнопки в том, что она заметнее, и почти не используется для перехода на страницу.',
   position: 'left',
   title: 'Название приложения в пять слов',
-  color: 'info'
+  color: 'success',
+  delay: 400,
+  show: true,
 };
 
 export const TooltipWithoutTitle = Template.bind({});
@@ -89,5 +125,5 @@ TooltipWithoutTitle.args = {
   text:
     'В современных веб-интерфейсах граница между кнопками и ссылками размыта. Ссылка также может запускать действие. Отличие кнопки в том, что она заметнее, и почти не используется для перехода на страницу.',
   position: 'right',
-  color: 'warning'
+  color: 'info',
 };
