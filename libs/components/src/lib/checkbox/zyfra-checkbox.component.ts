40b4f178f7cdcdfdabf2236d08fda9bebf1157fe
RustamI <jamakrus@gmail.com>
2022-04-19 09:03:18 +0000

    Release 0.7.0

--- libs/components/src/lib/checkbox/zyfra-checkbox.component.ts
+++ libs/components/src/lib/checkbox/zyfra-checkbox.component.ts
@@ -8,11 +8,25 @@ import { WrappedFormComponent } from '../@core/value-accessor/wrapped-form.compo
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ZyfraCheckboxComponent extends WrappedFormComponent implements ControlValueAccessor {
-  @Input() binary = true;
-  @Input() checkboxIcon = 'zyfra-icon selection-check-simple';
-  @Input() model: any;
-  @Input() disabled: boolean;
+  @Input() name: string = null;
+  @Input() value: any = null;
   @Input() label: string;
+  @Input() disabled: boolean;
+  @Input() binary: boolean = true;
+  @Input() tabindex: number = null;
+  @Input() inputId: string = null;
+  @Input() ariaLabelledBy: string = null;
+  @Input() ariaLabel: string = null;
+  @Input() style: object = null;
+  @Input() styleClass: string = null;
+  @Input() labelStyleClass: string = null;
+  @Input() checkboxIcon = 'zyfra-icon selection-check-simple';
+  @Input() readonly: boolean = false;
+  @Input() required: boolean = false;
+  @Input() trueValue: any = null;
+  @Input() falseValue: any = null;
+  @Input() model: any;
+
   @Output() modelChange = new EventEmitter<boolean | null>();
 
   public override setDisabledState(): void {
