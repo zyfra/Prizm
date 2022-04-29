4b835a428c88e838a617eafe93e3aa501f9b77c9
katkovergina <katkovergina@gmail.com>
2022-04-29 18:10:49 +0300

    PLATFORM01-13727: feat icons-16

--- libs/components/src/lib/icon/zyfra-icon.component.ts
+++ libs/components/src/lib/icon/zyfra-icon.component.ts
@@ -8,5 +8,5 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 })
 export class ZyfraIconComponent {
   @Input() iconClass: string = null;
-  @Input() ClassName: string;
+  @Input() className: string;
 }
