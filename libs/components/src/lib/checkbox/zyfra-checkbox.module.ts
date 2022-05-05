e3d615e19afd46a12cb30a8e8924f6522433d575
Ekaterina Kovergina <ekaterina.kovergina@idp.zyfra.com>
2022-05-05 20:07:53 +0300

    fix style, add ZyfraDisableControleModule

--- libs/components/src/lib/checkbox/zyfra-checkbox.module.ts
+++ libs/components/src/lib/checkbox/zyfra-checkbox.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CheckboxModule } from 'primeng/checkbox';
 import { ZyfraCheckboxComponent } from './zyfra-checkbox.component';
+import { ZyfraDisableControlModule } from '../@core/shared/zyfra-disable-control.module';
 
 @NgModule({
   declarations: [ZyfraCheckboxComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, CheckboxModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, CheckboxModule, ZyfraDisableControlModule],
   exports: [ZyfraCheckboxComponent],
 })
 export class ZyfraCheckBoxModule {}
