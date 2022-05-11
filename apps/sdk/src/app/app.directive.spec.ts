c5bd10bfef6e9a2c2cf84f16abfd2551fa962e1d
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2022-05-11 20:33:08 +0000

    release: 0.8.0

--- apps/sdk/src/app/app.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AppDirective } from './app.directive';
-
-describe('AppDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AppDirective();
-    expect(directive).toBeTruthy();
-  });
-});
