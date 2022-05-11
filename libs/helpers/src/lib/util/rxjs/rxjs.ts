9f4db6b384fa944c84a9aaf9d7f2650feb32a587
ZurabM <zurab@cnt.one>
2022-05-11 16:09:07 +0000

    Feat rxjs helpers

---
+++ libs/helpers/src/lib/util/rxjs/rxjs.ts
@@ -0,0 +1,27 @@
+import { Compare } from "../compare/compare";
+import {
+  filter,
+  map,
+} from 'rxjs/operators';
+import { MonoTypeOperatorFunction, OperatorFunction } from "rxjs";
+
+
+export function filterFalsy<T>(): MonoTypeOperatorFunction<T> {
+  return filter(Compare.isFalsy);
+}
+
+export function filterTruthy<T>(): MonoTypeOperatorFunction<T> {
+  return filter(Compare.isTruthy);
+}
+
+export function filterNotNullish<T>(): OperatorFunction<T, NonNullable<T>> {
+  return filter(Compare.isNotNullish);
+}
+
+export function filterNullish<T>(): OperatorFunction<T, null | undefined> {
+  return filter(Compare.isNullish);
+}
+
+export function mapUndefinedToNull<T>(): OperatorFunction<T, T | null> {
+  return map((v: T | undefined) => (Compare.isUndefined(v) ? null : (v as T)));
+}
