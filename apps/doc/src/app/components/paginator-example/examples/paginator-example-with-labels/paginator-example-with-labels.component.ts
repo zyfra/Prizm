dc4b65331b1cc36afb6f608e4afc4009bba0c53c
Khachatur Matevosyan <khachatur.matevosyan@idp.zyfra.com>
2022-06-17 10:42:43 +0000

    PLATFORM01-14787 Реализован компонент paginator

---
+++ apps/doc/src/app/components/paginator-example/examples/paginator-example-with-labels/paginator-example-with-labels.component.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'zui-paginator-example-with-labels',
+  templateUrl: './paginator-example-with-labels.component.html',
+  styleUrls: ['./paginator-example-with-labels.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PaginatorExampleWithLabelsComponent {}
