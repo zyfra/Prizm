09e2066d7322707e6342b1dff2f587c0046dae4e
Maryana Kabardina <maryana.kabardina@idp.zyfra.com>
2022-08-05 09:24:51 +0000

    PLATFORM01-15368 [Components] Поддержка реактивных форм

--- libs/components/src/lib/select-button/zyfra-select-button.module.ts
+++ libs/components/src/lib/select-button/zyfra-select-button.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ZyfraSelectButtonComponent } from './zyfra-select-button.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SelectButtonModule } from 'primeng/selectbutton';
 
 @NgModule({
   declarations: [ZyfraSelectButtonComponent],
-  imports: [SelectButtonModule],
+  imports: [SelectButtonModule, FormsModule, ReactiveFormsModule],
   exports: [ZyfraSelectButtonComponent],
 })
 export class ZyfraSelectButtonModule {}
