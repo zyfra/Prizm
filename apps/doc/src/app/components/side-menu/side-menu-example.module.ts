569c05c41d4cc855091862146a9b741be83bc1bd
Khachatur Matevosyan <khachatur.matevosyan@idp.zyfra.com>
2022-08-11 15:19:53 +0000

    fix(components/radio, component/checkbox, component/table, component/paginator, component/panel, component/input-chips) Исправлены критичные баги, добавлены компоненты в темной теме

--- apps/doc/src/app/components/side-menu/side-menu-example.module.ts
+++ apps/doc/src/app/components/side-menu/side-menu-example.module.ts
@@ -4,7 +4,7 @@ import { SideMenuExampleComponent } from './side-menu-example.component';
 import { generateRoutes, TuiAddonDocModule } from '@taiga-ui/addon-doc';
 import { RouterModule } from '@angular/router';
 import { SideMenuExampleBasicComponent } from './examples/side-menu-example-basic/side-menu-example-basic.component';
-import { ZuiIconModule, ZuiPanelModule } from '@digital-plant/zui-components';
+import { ZuiIconModule, ZuiInputTextModule, ZuiPanelModule } from '@digital-plant/zui-components';
 
 @NgModule({
   declarations: [SideMenuExampleComponent, SideMenuExampleBasicComponent],
@@ -14,6 +14,7 @@ import { ZuiIconModule, ZuiPanelModule } from '@digital-plant/zui-components';
     ZuiIconModule,
     RouterModule.forChild(generateRoutes(SideMenuExampleComponent)),
     ZuiPanelModule,
+    ZuiInputTextModule,
   ],
 })
 export class SideMenuExampleModule {}
--- libs/next/src/lib/components/tabs/tabs.module.ts
+++ libs/next/src/lib/components/tabs/tabs.module.ts
@@ -5,10 +5,19 @@ import { TabComponent } from './components/tab.component';
 import { TabsComponent } from './tabs.component';
 import { ZuiDropdownHostModule } from '../dropdowns/dropdown-host';
 import { ZuiButtonModule } from '../button';
+import { ZuiDropdownControllerModule } from '../../directives';
+import { ZuiDataListModule } from '../data-list';
 
 @NgModule({
   declarations: [TabsComponent, TabComponent],
-  imports: [CommonModule, ZuiIconModule, ZuiDropdownHostModule, ZuiButtonModule],
+  imports: [
+    CommonModule,
+    ZuiIconModule,
+    ZuiDropdownHostModule,
+    ZuiButtonModule,
+    ZuiDropdownControllerModule,
+    ZuiDataListModule,
+  ],
   exports: [TabsComponent, TabComponent],
 })
 export class ZuiTabsModule {}
