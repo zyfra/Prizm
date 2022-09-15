ad51b4f727fb7f0217daf12719907a8f9b909cef
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2022-09-15 13:35:06 +0000

    Release 0.13.4

--- libs/schematics/src/ng-add/consts.ts
+++ libs/schematics/src/ng-add/consts.ts
@@ -11,17 +11,17 @@ interface ImportingModule {
 export const MAIN_PACKAGES: ReadonlyArray<Package> = [
   {
     name: '@digital-plant/zyfra-components',
-    version: '0.13.3',
+    version: '0.13.4',
   },
   {
     name: '@digital-plant/zyfra-helpers',
-    version: '0.13.3',
+    version: '1.0.0-beta.6',
   },
 ];
 
 export const INSTALL_PACKAGE: Readonly<Package> = {
   name: '@digital-plant/zyfra-ui',
-  version: '0.13.3',
+  version: '0.13.4',
 };
 
 export const MAIN_MODULES: ReadonlyArray<ImportingModule> = [
