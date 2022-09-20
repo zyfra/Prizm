10361aaea0ee304e702e8708ad537c07c41b7bbf
Konstantin Khimenkov <konstantin.khimenkov@zyfra.com>
2022-09-20 11:20:12 +0300

    fix(zyfra-base-table.directive): export interface LazyLoadEvent

--- libs/components/src/lib/@core/base-table/shared-table.types.ts
+++ libs/components/src/lib/@core/base-table/shared-table.types.ts
@@ -1,1 +1,13 @@
+import { SortMeta } from 'primeng/api';
+
 export type SortOrder = -1 | 1;
+
+export interface LazyLoadEvent {
+  first: number;
+  rows: number;
+  sortField: string;
+  sortOrder: SortOrder;
+  multiSortMeta: SortMeta[];
+  filters: Record<string, unknown>;
+  globalFilter: unknown;
+}
