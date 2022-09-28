1a0f0b99bddbf4b4ca9f1fb85c72b933ae14e8e1
Khachatur Matevosyan <khachatur.matevosyan@idp.zyfra.com>
2022-09-28 07:25:22 +0000

    fix(components/nav-menu): Фикс бага с отображением иконок, фикс бага с границей.

--- apps/doc/src/app/components/breadcrumbs/breadcrumbs-example.component.ts
+++ apps/doc/src/app/components/breadcrumbs/breadcrumbs-example.component.ts
@@ -19,15 +19,21 @@ export class BreadcrumbsExampleComponent {
   ];
 
   public readonly breadcrumbsBasicExample: TuiDocExample = {
-    TypeScript: import('!!raw-loader!./examples/breadcrumbs-example-basic/breadcrumbs-example-basic.component'),
-    HTML: import('!!raw-loader!./examples/breadcrumbs-example-basic/breadcrumbs-example-basic.component.html'),
-    LESS: import('./examples/breadcrumbs-example-basic/breadcrumbs-example-basic.component.less?raw'),
+    TypeScript: import(
+      '!!raw-loader!.//examples/breadcrumbs-example-basic/breadcrumbs-example-basic.component'
+    ),
+    HTML: import(
+      '!!raw-loader!.//examples/breadcrumbs-example-basic/breadcrumbs-example-basic.component.html'
+    ),
   };
 
   public readonly breadcrumbsWithIconExample: TuiDocExample = {
-    TypeScript: import('!!raw-loader!./examples/breadcrumbs-example-with-icon/breadcrumbs-example-with-icon.component'),
-    HTML: import('!!raw-loader!./examples/breadcrumbs-example-with-icon/breadcrumbs-example-with-icon.component.html'),
-    LESS: import('./examples/breadcrumbs-example-with-icon/breadcrumbs-example-with-icon.component.less?raw'),
+    TypeScript: import(
+      '!!raw-loader!.//examples/breadcrumbs-example-with-icon/breadcrumbs-example-with-icon.component'
+    ),
+    HTML: import(
+      '!!raw-loader!.//examples/breadcrumbs-example-with-icon/breadcrumbs-example-with-icon.component.html'
+    ),
   };
 
   public readonly setupModule: RawLoaderContent = import('!!raw-loader!./examples/setup-module.md');
