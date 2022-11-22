29d2feb32dbbef3549c8492c551a79240da5fc6e
Zurab <zurab.magomadov@idp.zyfra.com>
2022-11-22 10:17:54 +0300

    restore store

---
+++ libs/next/src/lib/components/stepper/stepper.component.ts
@@ -0,0 +1,67 @@
+import {
+  AfterContentInit,
+  Component,
+  ContentChildren,
+  EventEmitter,
+  Input,
+  Output,
+  QueryList,
+  TemplateRef,
+} from '@angular/core';
+
+import { merge, Observable } from 'rxjs';
+import { map, startWith, switchMap } from 'rxjs/operators';
+import { PrizmStepperStepDirective } from './stepper-step.directive';
+
+@Component({
+  selector: 'prizm-stepper',
+  templateUrl: './stepper.component.html',
+  styleUrls: ['./stepper.component.less'],
+  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+  host: {
+    class: 'prizm-stepper',
+    '[class.vertical]': 'vertical',
+  },
+})
+export class PrizmStepperComponent implements AfterContentInit {
+  @Input() title = '';
+
+  @Input() currentStep = 1;
+
+  @Input() vertical = false;
+
+  @Input() stepsSize = 'auto';
+
+  @Output() selectStep = new EventEmitter<number>();
+
+  @ContentChildren(PrizmStepperStepDirective)
+  prizmStepperStepDirectiveQL!: QueryList<PrizmStepperStepDirective>;
+
+  steps$!: Observable<PrizmStepperStepDirective[]>;
+
+  get currentStepTemplate(): TemplateRef<any> {
+    const currentStep = this.prizmStepperStepDirectiveQL.find(
+      stepDirective => stepDirective.index === this.currentStep
+    );
+    return currentStep.template;
+  }
+
+  public ngAfterContentInit(): void {
+    const steps$ = this.prizmStepperStepDirectiveQL.changes.pipe(
+      startWith<QueryList<PrizmStepperStepDirective>>(this.prizmStepperStepDirectiveQL)
+    );
+
+    const stepsInnerStateChanged$ = merge(
+      ...this.prizmStepperStepDirectiveQL.toArray().map(stepDirective => stepDirective.stateChanged)
+    );
+
+    this.steps$ = merge(steps$, stepsInnerStateChanged$.pipe(switchMap(() => steps$))).pipe(
+      map(ql => ql.toArray().sort(item => item.index))
+    );
+  }
+
+  public onSelectStep(step: number): void {
+    this.selectStep.next(step);
+  }
+}
+
