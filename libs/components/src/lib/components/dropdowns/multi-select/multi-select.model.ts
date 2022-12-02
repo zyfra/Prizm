96b27d42d166879cb3ace8a749109e97e3951b32
Rustam Imaikin <rustam.imaikin@idp.zyfra.com>
2022-12-02 12:37:55 +0300

    chore: rename 'next' to 'components'

---
+++ libs/components/src/lib/components/dropdowns/multi-select/multi-select.model.ts
@@ -0,0 +1,12 @@
+export type PrizmMultiSelectSearchMatcher<T> = (searchValue: string, item: T) => boolean;
+export type PrizmMultiSelectIdentityMatcher<T> = (item1: T, item2: T) => boolean;
+
+export type PrizmMultiSelectItemWithChecked<T> = {
+  checked: boolean,
+  indeterminate?: boolean,
+  obj: T,
+  stringify: string
+}
+
+export type PrizmMultiSelectItemStringifyFunc<T> = (i: PrizmMultiSelectItemStringifyItem<T>, nullContent?: string) => string
+export type PrizmMultiSelectItemStringifyItem<T> = Omit<PrizmMultiSelectItemWithChecked<T>, 'stringify'>
--- libs/next/src/lib/directives/mutation-observer/mutation-observer.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { PrizmMutationObserveDirective } from './mutation-observer.directive';
-
-@NgModule({
-  declarations: [PrizmMutationObserveDirective],
-  exports: [PrizmMutationObserveDirective],
-})
-export class PrizmMutationObserveModule {}
