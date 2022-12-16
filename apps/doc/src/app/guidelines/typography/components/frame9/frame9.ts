60689ae314e6b38d08c8d4d7ab130e526ede58aa
Yakov <korolev.yasha@gmail.com>
2022-12-16 04:42:37 +0300

    create window: create guidelines (typography)

---
+++ apps/doc/src/app/guidelines/typography/components/frame9/frame9.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-frame9',
+  templateUrl: 'frame9.html',
+  styleUrls: ['frame9.css'],
+})
+export class Frame9 {
+  raw1s6o: string = ' '
+  rawmj2n: string = ' '
+
+  constructor() {}
+}
