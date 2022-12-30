602de65f0788391ba4a2ddd2cc58db77dbe060bf
Zurab <zurab.magomadov@idp.zyfra.com>
2022-12-30 16:54:00 +0300

    feat(charts): stage 2

--- apps/doc/src/app/charts/treemap/examples/base/prizm-charts-treemap-example.component.ts
+++ apps/doc/src/app/charts/treemap/examples/base/prizm-charts-treemap-example.component.ts
@@ -12,4 +12,30 @@ import { Component } from '@angular/core';
     `
   ],
 })
-export class PrizmChartsTreemapExampleComponent {}
+export class PrizmChartsTreemapExampleComponent {
+  public data =  {
+    name: 'root',
+    children: [
+      { name: 'Категория 1', value: 560 },
+      { name: 'Категория 2', value: 500 },
+      { name: 'Категория 3', value: 150 },
+      { name: 'Категория 4', value: 140 },
+      { name: 'Категория 5', value: 115 },
+      { name: 'Категория 6', value: 95 },
+      { name: 'Категория 7', value: 90 },
+      { name: 'Категория 8', value: 75 },
+      { name: 'Категория 9', value: 98 },
+      { name: 'Категория 10', value: 60 },
+      { name: 'Категория 11', value: 45 },
+      { name: 'Категория 12', value: 40 },
+      { name: 'Категория 13', value: 40 },
+      { name: 'категория 14', value: 35 },
+      { name: 'Категория 15', value: 40 },
+      { name: 'Категория 16', value: 40 },
+      { name: 'Категория 17', value: 40 },
+      { name: 'Категория 18', value: 30 },
+      { name: 'категория 19', value: 28 },
+      { name: 'категория 20', value: 16 },
+    ],
+  };
+}
