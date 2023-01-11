d28bd6becbdf4b9eb3b362b547cb5e771eff754d
ZurabM <zurab@cnt.one>
2023-01-11 12:43:26 +0300

    fix(component/date): fix style in dark

--- apps/doc/src/app/components/panel/panel-example.module.ts
+++ apps/doc/src/app/components/panel/panel-example.module.ts
@@ -2,22 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PanelExampleComponent } from './panel-example.component';
 import { PanelExampleBasicComponent } from './examples/panel-example-basic/panel-example-basic.component';
-import { prizmDocGenerateRoutes, PrizmAddonDocModule } from '@prizm-ui/doc';
+import { PrizmAddonDocModule, prizmDocGenerateRoutes } from '@prizm-ui/doc';
 import { RouterModule } from '@angular/router';
 import {
+  PrizmBreadcrumbsModule,
   PrizmButtonModule,
   PrizmIconModule,
   PrizmPanelModule,
-  PrizmBreadcrumbsModule,
   PrizmTabsModule,
   PrizmToggleModule,
 } from '@prizm-ui/components';
 import { PanelExampleWithTabsComponent } from './examples/panel-example-with-tabs/panel-example-with-tabs.component';
 import { PanelExampleWithPagesComponent } from './examples/panel-example-with-pages/panel-example-with-pages.component';
-import { PanelExampleWithBreadcrumbsComponent } from './examples/panel-example-with-breadcrumbs/panel-example-with-breadcrumbs.component';
+import {
+  PanelExampleWithBreadcrumbsComponent,
+} from './examples/panel-example-with-breadcrumbs/panel-example-with-breadcrumbs.component';
 import { PanelExampleWithBackComponent } from './examples/panel-example-with-back/panel-example-with-back.component';
-import { PanelExampleWithInstrumentsSimpleComponent } from './examples/panel-example-with-instruments-simple/panel-example-with-instruments-simple.component';
-import { PanelExampleWithInstrumentsHardComponent } from './examples/panel-example-with-instruments-hard/panel-example-with-instruments-hard.component';
+import {
+  PanelExampleWithInstrumentsSimpleComponent,
+} from './examples/panel-example-with-instruments-simple/panel-example-with-instruments-simple.component';
+import {
+  PanelExampleWithInstrumentsHardComponent,
+} from './examples/panel-example-with-instruments-hard/panel-example-with-instruments-hard.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
