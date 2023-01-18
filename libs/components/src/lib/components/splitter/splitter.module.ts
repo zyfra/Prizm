34d12e0882cd9c402dc3f7c06b058a80d1be0be7
skkonstantin <skok.konstantin@gmail.com>
2023-01-18 11:31:32 +0000

    refactor (components/splitter)  события изменения размера. фикс бага с классами

--- libs/components/src/lib/components/splitter/splitter.module.ts
+++ libs/components/src/lib/components/splitter/splitter.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ResizeObserverModule } from '@ng-web-apis/resize-observer';
 import { PrizmSplitterAreaComponent } from './splitter-area.component';
 import { PrizmSplitterGutterComponent } from './splitter-gutter.component';
 
 import { PrizmSplitterComponent } from './splitter.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ResizeObserverModule],
   declarations: [PrizmSplitterComponent, PrizmSplitterAreaComponent, PrizmSplitterGutterComponent],
   exports: [PrizmSplitterComponent, PrizmSplitterAreaComponent],
 })
