a33f78a7b826d7a9aee2cded58d1567714dce44e
unknown <alexander.yastrebov@idp.zyfra.com>
2023-02-17 16:18:57 +0300

    test(doc): add urls for all components

--- apps/doc/pw/const.ts
+++ apps/doc/pw/const.ts
@@ -2,28 +2,410 @@ import { PrizmPlaywrightDemoItem } from './model';
 
 export const URLS_FOR_DEMO_CHECK: PrizmPlaywrightDemoItem[] = [
   {
-    url: '/components/accordion',
-    hasLiveDemo: true,
-    hasExamples: true,
+  url: '/components/accordion',
+   hasLiveDemo: true,
+   hasExamples: true,
   },
+
   {
-    url: '/components/button',
-    hasLiveDemo: true,
-    hasExamples: true,
+  url: '/components/breadcrumbs',
+   hasLiveDemo: true,
+   hasExamples: true,
   },
+
   {
-    url: '/components/breadcrumbs',
-    hasLiveDemo: true,
-    hasExamples: true,
+  url: '/components/button',
+   hasLiveDemo: true,
+   hasExamples: true,
   },
+
   {
-    url: '/components/split-button',
-    hasLiveDemo: true,
-    hasExamples: true,
+  url: '/components/split-button',
+   hasLiveDemo: true,
+   hasExamples: true,
   },
+
   {
-    url: '/components/icon-button',
-    hasLiveDemo: true,
-    hasExamples: true,
+  url: '/components/icon-button',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/calendar',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/calendar-month',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/calendar-range',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/card',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/checkbox',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/confirm-popup',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/cron',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dialogs/dialog',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dialogs/sidebar',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dialogs/confirm-dialog',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dropdowns/dropdown-host',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dropdowns/select',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/dropdowns/multi-select',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/error-page',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/file-upload',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/grids',
+   hasLiveDemo: false,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/hint',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/old-icon',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/flag-icons',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/icon',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/indicators',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date-time',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date-time-range',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-time',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-month',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-month-range',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date-range',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date-relative',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-date-multi',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/textarea',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-chips',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-number',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-mask',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/input-password',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/carousel',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/loader',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/navigation',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/nav-menu',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/paginator',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/panel',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/progress-line-bar',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/progress-circle-bar',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/progress-line-segmented',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/radio-button',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/scrollbar',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/shadow',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/side-menu',
+   hasLiveDemo: false,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/skeleton',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/slider',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/splitter',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/stepper',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/switcher',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/table',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/table-old',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/tabs',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/toast',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/toggle',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/tooltip',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/tree',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/widget',
+   hasLiveDemo: true,
+   hasExamples: true,
+  },
+
+  {
+  url: '/components/zoom-control',
+   hasLiveDemo: true,
+   hasExamples: true,
   },
 ];
