eab80c1c0b5609bcafd346fc928227ad491a7a08
unknown <alexander.yastrebov@idpllc.ru>
2023-02-22 11:52:05 +0300

    test(doc): fix & modify console errors test

--- playwright.config.base.ts
+++ playwright.config.base.ts
@@ -1,12 +1,12 @@
-import { PlaywrightTestConfig } from '@playwright/test';
+import { defineConfig } from '@playwright/test';
 
 const baseURL = process.env.E2E_BASE_URL || 'http://localhost:4200/';
 
-export const baseConfig: PlaywrightTestConfig = {
+export default defineConfig({
   retries: 3,
   maxFailures: 2,
   timeout: 120000,
   use: {
     baseURL,
   },
-};
+});
