aeb8bb9d04c4b8c054fefae93fbdecce043ec0c2
Yakov <korolev.yasha@gmail.com>
2023-03-17 13:29:52 +0300

    fix: remove trivially type annotation

--- libs/components/src/lib/components/paginator/prizm-paginator.component.ts
+++ libs/components/src/lib/components/paginator/prizm-paginator.component.ts
@@ -21,7 +21,7 @@ export class PrizmPaginatorComponent {
   @Input() public pageLinkSize: number | null = null;
   @Input() public showMoreDisabled = false;
   // disabled
-  @Input() public disabled: boolean = true;
+  @Input() public disabled = true;
 
   // Сколько данных в одном пакете
   @Input() public rows: number | null = null;
