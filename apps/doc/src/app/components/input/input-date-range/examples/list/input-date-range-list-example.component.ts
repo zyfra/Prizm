17edffe5d88fd9eee6429135d37c5b0f7868c506
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-03-30 15:57:44 +0300

    feat(components/switcher): add disabled and add support template icon

---
+++ apps/doc/src/app/components/input/input-date-range/examples/list/input-date-range-list-example.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { PrizmDay, PrizmDayRange } from '@prizm-ui/components';
+import { PrizmDayRangePeriod } from '../../../../../../../../../libs/components/src/lib/@core/classes/day-range-period';
+
+@Component({
+  selector: 'prizm-input-date-range-base-example',
+  templateUrl: './input-date-range-base-example.component.html',
+  styles: [
+    `
+      .box {
+        display: flex;
+        gap: 1rem;
+      }
+    `,
+  ],
+})
+export class PrizmInputDateRangeBaseExampleComponent {
+  readonly value = new FormControl(new PrizmDayRange(new PrizmDay(2018, 2, 10), new PrizmDay(2018, 3, 20)));
+  readonly min = new PrizmDay(2000, 2, 20);
+
+  readonly max = new PrizmDay(2040, 2, 20);
+  readonly items = [
+    new PrizmDayRangePeriod(
+      new PrizmDayRange(
+        new PrizmDay(2018, 1, 1),
+        new PrizmDay(2019, 12, 31)
+      ),
+      '2018-2019'
+    )
+  ]
+}
