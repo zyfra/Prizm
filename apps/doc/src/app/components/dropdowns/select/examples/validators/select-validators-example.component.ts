2f2df806cd3fb6a76d297836dd71bbe105369897
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-03-31 16:10:34 +0300

    fix(comopnents/select): validator required does not work

---
+++ apps/doc/src/app/components/dropdowns/select/examples/validators/select-validators-example.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'prizm-select-base-example',
+  templateUrl: './select-base-example.component.html',
+  styles: [
+    `
+      .box {
+        display: flex;
+        gap: 1rem;
+      }
+    `,
+  ],
+})
+export class PrizmSelectBaseExampleComponent {
+  readonly items = [
+    'One',
+    'Two',
+    'Three',
+    'Very long text with a lot of characters and spaces and other stuff and things',
+  ];
+  readonly control = new FormControl(this.items[1], [Validators.required]);
+
+  public setDefaultValue(): void {
+    this.control.setValue(this.items[0], { emitEvent: false });
+  }
+}
