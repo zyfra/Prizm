40bedb6790e864a93790c3fc40ed8ff25aab3326
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-04-14 18:00:01 +0300

    fix: multi-select touched

--- apps/doc/src/app/components/input/input-date-time/examples/base/input-date-time-base-example.component.ts
+++ apps/doc/src/app/components/input/input-date-time/examples/base/input-date-time-base-example.component.ts
@@ -15,5 +15,13 @@ import { PrizmDay, PrizmTime } from '@prizm-ui/components';
   ],
 })
 export class PrizmInputDateTimeBaseExampleComponent {
-  public readonly value = new FormControl([new PrizmDay(2017, 2, 15), new PrizmTime(12, 30)]);
+  public readonly value = new FormControl([new PrizmDay(2022, 2, 15), new PrizmTime(12, 30)]);
+
+  public setDefaultValue(): void {
+    this.value.setValue([new PrizmDay(2022, 1, 1), new PrizmTime(6, 0)]);
+  }
+
+  public clear(): void {
+    this.value.setValue(null);
+  }
 }
