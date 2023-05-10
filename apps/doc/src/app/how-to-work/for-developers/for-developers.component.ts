0b544589110ebbad3bb4bacd55c8dbc0a546d4f9
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-05-10 10:02:03 +0300

    fix(doc/getting-started): corrected path for connecting icons

--- apps/doc/src/app/how-to-work/for-developers/for-developers.component.ts
+++ apps/doc/src/app/how-to-work/for-developers/for-developers.component.ts
@@ -10,17 +10,17 @@ export class ForDevelopersComponent {
   public readonly joinSvgFonts = `
   "styles": [
      ...
-     "@prizm-ui/icons/base/src/styles/styles.less"
+     "@prizm-ui/icons/src/styles/styles.less"
    ],";
    `;
-  public readonly joinSvgFontsForImport = `@import "~@prizm-ui/icons/base/src/styles/styles.less";
+  public readonly joinSvgFontsForImport = `@import "~@prizm-ui/icons/src/styles/styles.less";
    `;
   public readonly updateAngularStylesFileCode = `
  // (Required) Add for use our theme
  @import "~@prizm-ui/theme/src/styles/styles.less";
  // (Optional) Add only for use our components library
  @import "~@prizm-ui/components/src/styles/styles.less";
- 
+
  // (Optional) Add only for use our old icon set (deprecated)
  @import "~@prizm-ui/components/src/styles/icons/icons.less";
  @import "~@prizm-ui/components/src/styles/icons-16/icons-16.less";
