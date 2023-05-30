9b4156af804f17046a325cd3b1d5e5e01b6bd256
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-05-30 18:26:39 +0300

    feat(components/cron): add min/max for start and end date

--- apps/doc/src/app/components/input/input-select/examples/base/select-base-example.component.ts
+++ apps/doc/src/app/components/input/input-select/examples/base/select-base-example.component.ts
@@ -16,29 +16,12 @@ import { PrizmSelectStringify } from '@prizm-ui/components';
 })
 export class PrizmSelectBaseExampleComponent {
   readonly items = [
-    '',
-    false,
-    true,
-    0,
     'One',
     'Two',
     'Three',
     'Very long text with a lot of characters and spaces and other stuff and things',
   ];
   readonly control = new UntypedFormControl(this.items[1], [Validators.required]);
-
-  readonly stringify: PrizmSelectStringify<string | number | boolean> = (item: string | number | boolean) => {
-    switch (typeof item) {
-      case 'string':
-        return item || 'Пустая строка';
-      case 'number':
-        return item.toString();
-      case 'boolean':
-        return item ? 'TRUE' : 'FALSY';
-    }
-
-    return item;
-  };
 
   public setDefaultValue(): void {
     this.control.setValue(this.items[0], { emitEvent: false });
