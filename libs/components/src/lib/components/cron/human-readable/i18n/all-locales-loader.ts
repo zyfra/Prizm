e65b7b2779c4f1918d60108f417414f332ed3579
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-06-19 04:00:36 +0300

    fix: lint errors

--- libs/components/src/lib/components/cron/human-readable/i18n/all-locales-loader.ts
+++ libs/components/src/lib/components/cron/human-readable/i18n/all-locales-loader.ts
@@ -4,7 +4,8 @@ import * as allLocales from './all-locales';
 export class PrizmCronHRAllLocalesLoader {
   public load(availableLocales: { [name: string]: PrizmCronHRLocale }) {
     for (const property in allLocales) {
-      // eslint-disable-next-line no-prototype-builtins
+      // eslint-disable-next-line no-prototype-builtins,@typescript-eslint/ban-ts-comment
+      // @ts-ignore
       if (typeof allLocales[property] !== 'undefined') {
         availableLocales[property] = new (allLocales as any)[property]() as PrizmCronHRLocale;
       }
--- libs/components/src/lib/components/input/input-date-time-range/model.ts
+++ libs/components/src/lib/components/input/input-date-time-range/model.ts
@@ -1,3 +1,5 @@
-import { PrizmDateTime, PrizmDay, PrizmTime } from '@prizm-ui/components';
+import { PrizmDateTime } from '../../../@core/date-time/date-time';
+import { PrizmDay } from '../../../@core/date-time/day';
+import { PrizmTime } from '../../../@core/date-time/time';
 
 export type PrizmDateTimeMinMax = PrizmDay | [PrizmDay, PrizmTime] | PrizmDateTime;
