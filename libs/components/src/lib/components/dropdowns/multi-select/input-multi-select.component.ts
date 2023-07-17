b98eed3ccbcb9fb1de916ca9a06e9eaf4df59c89
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-07-17 07:39:55 +0300

    feat(components/input-multi-select): set touched state only after choose or clear

--- libs/components/src/lib/components/dropdowns/multi-select/input-multi-select.component.ts
+++ libs/components/src/lib/components/dropdowns/multi-select/input-multi-select.component.ts
@@ -310,6 +310,7 @@ export class PrizmInputMultiSelectComponent<T> extends PrizmInputNgControl<T[]> 
   public select(item: PrizmMultiSelectItemWithChecked<T>): void {
     const newItemState = !item.checked;
     let values: T[];
+    this.markAsTouched();
     if (this.isSelectAllItem(item)) {
       values = newItemState ? [...this.items] : [];
     } else {
