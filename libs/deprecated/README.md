b429d596b4d049d482b556a227868e593d41df58
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-07-18 09:56:38 +0300

    chore: fix mr

--- libs/deprecated/README.md
+++
@@ -1,7 +0,0 @@
-# deprecated
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test deprecated` to execute the unit tests.
--- libs/deprecated/src/lib/components/table-old/directives/reordable-row-handler.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, Input } from '@angular/core';
-import { ReorderableRowHandle } from 'primeng/table';
-
-@Directive({
-  selector: '[prizmReordableRowHandler]',
-})
-export class PrizmReordableRowHandlerDirective extends ReorderableRowHandle {
-  @Input('prizmReordableRowHandler') index: number;
-}
