9b3a1c478430ba172551bcc30cabfd20bfbadd68
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-07-27 16:51:33 +0300

    fix(doc): changed class name

--- libs/components/src/lib/modules/overlay/overlay.component.ts
+++ libs/components/src/lib/modules/overlay/overlay.component.ts
@@ -131,6 +131,7 @@ export class PrizmOverlayComponent implements OnInit, AfterViewInit, OnDestroy {
   private setPos(): void {
     this.updatePos();
 
+    /** if change position after render > we re-update position */
     timer(0)
       .pipe(
         tap(() => this.updatePos()),
