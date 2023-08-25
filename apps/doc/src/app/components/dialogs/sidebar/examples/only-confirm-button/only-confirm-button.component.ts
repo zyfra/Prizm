d31829b8ce6035e37d0724dcf14cfffcb66fe0fa
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-08-25 15:39:46 +0300

    feat(doc/sidebar): example was added with only confirm button

---
+++ apps/doc/src/app/components/dialogs/sidebar/examples/only-confirm-button/only-confirm-button.component.ts
@@ -0,0 +1,44 @@
+import { Component, Inject, TemplateRef, ViewChild } from '@angular/core';
+import { PrizmSidebarService, PrizmOverlayInsidePlacement } from '@prizm-ui/components';
+
+@Component({
+  selector: 'prizm-sidebar-only-confirm-button-example',
+  templateUrl: './only-confirm-button.component.html',
+  styles: [
+    `
+      .box {
+        display: flex;
+        gap: 1rem;
+      }
+    `,
+  ],
+})
+export class PrizmSidebarOnlyConfirmButtonExampleComponent {
+  @ViewChild('contentExample') contentExample: TemplateRef<any>;
+  public positionVariants: PrizmOverlayInsidePlacement[] = [
+    PrizmOverlayInsidePlacement.LEFT,
+    PrizmOverlayInsidePlacement.RIGHT,
+  ];
+  public position: PrizmOverlayInsidePlacement = this.positionVariants[1];
+  public backdrop = false;
+  public dismissible = false;
+
+  constructor(@Inject(PrizmSidebarService) private readonly sidebarService: PrizmSidebarService) {}
+
+  public show(): void {
+    this.sidebarService
+      .open(this.contentExample, {
+        closeable: true,
+        header: 'Header',
+        width: '400px',
+        cancelButton: null,
+        supportButton: null,
+        confirmButton: 'Закрыть',
+        position: this.position,
+        backdrop: this.backdrop,
+        dismissible: this.dismissible,
+        size: 'm',
+      })
+      .subscribe();
+  }
+}
