0bd74f152fc522408253c7b2192661520d2592bd
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2023-09-21 23:14:29 +0300

    feat: control reset added

--- apps/doc/src/app/components/dialogs/dialog/examples/result/dialog-result-handling-example.component.ts
+++ apps/doc/src/app/components/dialogs/dialog/examples/result/dialog-result-handling-example.component.ts
@@ -38,6 +38,7 @@ export class PrizmDialogServiceResultHandlingExampleComponent {
       })
       .subscribe(result => {
         this.result = result;
+        this.control.reset();
       });
   }
 }
