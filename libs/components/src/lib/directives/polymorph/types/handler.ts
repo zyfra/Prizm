9cc98c1ff16be0570a61cf85784eec2189b010f3
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-10-09 16:00:33 +0300

    fix(components/polymorph): build error #738

--- libs/components/src/lib/directives/polymorph/types/handler.ts
+++ libs/components/src/lib/directives/polymorph/types/handler.ts
@@ -2,4 +2,4 @@
  * A handler function receiving context as input and returning a primitive
  */
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type PolymorphHandler<C extends object> = (context: C) => string | number;
+export type PolymorphHandler<C = any> = (context: C) => string | number;
