b9245f88cb36cee44aed210e7c0a6b00d9eee465
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-01 10:56:07 +0300

    chore: fix build errors

--- libs/components/src/lib/components/dropdowns/dropdown-host/dropdown-host.module.ts
+++ libs/components/src/lib/components/dropdowns/dropdown-host/dropdown-host.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PrizmDropdownHostControlDirective } from './dropdown-host-control.directive';
 import { PrizmDropdownHostComponent } from './dropdown-host.component';
-import { PrizmDropdownZoneModule } from '@prizm-ui/components';
+import { PrizmDropdownZoneModule } from '../../../directives/event-zone';
 
 /**
  * @deprecated
