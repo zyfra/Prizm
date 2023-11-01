5a45937b850f5af5c2ff3dae8c386527a9697a6f
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-01 17:22:15 +0300

    fix: build errors

--- libs/components/src/lib/components/dropdowns/select/index.ts
+++ libs/components/src/lib/components/dropdowns/select/index.ts
@@ -4,5 +4,6 @@ export * from './select.options';
 export * from './select.model';
 export * from './input-select.component';
 export * from './input-select.module';
+export * from './input-select-item.component';
 export * from './input-select-data-list.directive';
 export * from './input-select-option.service';
