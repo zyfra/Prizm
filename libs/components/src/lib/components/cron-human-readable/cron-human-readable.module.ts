ef7a7cdf357ce92af26ec3d056b7d31afe602d1c
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-07 10:06:36 +0300

    feat(components/cron-hr): to standalone

--- libs/components/src/lib/components/cron-human-readable/cron-human-readable.module.ts
+++ libs/components/src/lib/components/cron-human-readable/cron-human-readable.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PrizmCronHumanReadablePipe } from './pipe/cron-human-readable.pipe';
 
+/**
+ * @deprecated
+ * use standalone
+ * */
 @NgModule({
-  declarations: [PrizmCronHumanReadablePipe],
+  imports: [PrizmCronHumanReadablePipe],
   exports: [PrizmCronHumanReadablePipe],
 })
 export class PrizmCronHumanReadableModule {}
