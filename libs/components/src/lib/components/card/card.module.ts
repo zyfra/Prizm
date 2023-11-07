f2323debb58838c40b3131038e0509cd5233f393
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-07 09:51:08 +0300

    feat(components/card): to standalone

--- libs/components/src/lib/components/card/card.module.ts
+++ libs/components/src/lib/components/card/card.module.ts
@@ -3,9 +3,12 @@ import { NgModule } from '@angular/core';
 import { PrizmCardComponent } from './card.component';
 import { PrizmShadowModule } from '../../directives/shadow';
 
+/**
+ * @deprecated
+ * user standalone
+ * */
 @NgModule({
-  imports: [CommonModule, PrizmShadowModule],
-  declarations: [PrizmCardComponent],
+  imports: [PrizmCardComponent],
   exports: [PrizmCardComponent],
 })
 export class PrizmCardModule {}
