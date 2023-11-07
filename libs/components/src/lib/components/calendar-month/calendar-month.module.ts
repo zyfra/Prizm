26c1deed7f41d572750cb85db6b858ddde795623
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2023-11-07 23:37:41 +0300

    feat: calendars v3 colors #987

--- libs/components/src/lib/components/calendar-month/calendar-month.module.ts
+++ libs/components/src/lib/components/calendar-month/calendar-month.module.ts
@@ -1,4 +1,14 @@
 import { NgModule } from '@angular/core';
+<<<<<<< HEAD
+=======
+import { PrizmLetModule } from '@prizm-ui/helpers';
+import { PrizmFocusableModule } from '../../directives/focusable/focusable.module';
+import { PrizmHoveredModule } from '../../directives/hovered/hovered.module';
+import { PrizmPressedModule } from '../../directives/pressed/pressed.module';
+import { PrizmPrimitiveSpinButtonModule } from '../internal/primitive-spin-button/primitive-spin-button.module';
+import { PrizmPrimitiveYearPickerModule } from '../internal/primitive-year-picker/primitive-year-picker.module';
+import { PrizmScrollbarModule } from '../scrollbar/scrollbar.module';
+>>>>>>> d1d815f8e (feat: calendars v3 colors #987)
 import { PrizmCalendarMonthComponent } from './calendar-month.component';
 
 
@@ -7,7 +17,22 @@ import { PrizmCalendarMonthComponent } from './calendar-month.component';
  * use standalone
  * */
 @NgModule({
+<<<<<<< HEAD
   imports: [PrizmCalendarMonthComponent],
+=======
+  imports: [
+    CommonModule,
+    PrizmStopPropagationModule,
+    PrizmPrimitiveYearPickerModule,
+    PrizmPrimitiveSpinButtonModule,
+    PrizmScrollbarModule,
+    PrizmLetModule,
+    PrizmHoveredModule,
+    PrizmPressedModule,
+    PrizmFocusableModule,
+  ],
+  declarations: [PrizmCalendarMonthComponent],
+>>>>>>> d1d815f8e (feat: calendars v3 colors #987)
   exports: [PrizmCalendarMonthComponent],
 })
 export class PrizmCalendarMonthModule {}
--- libs/components/src/lib/components/internal/primitive-year-month-pagination/primitive-year-month-pagination.module.ts
+++ libs/components/src/lib/components/internal/primitive-year-month-pagination/primitive-year-month-pagination.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { PrizmPrimitiveYearMonthPaginationComponent } from './primitive-year-month-pagination.component';
+<<<<<<< HEAD
+=======
+import { PrizmPrimitiveSpinButtonModule } from '../primitive-spin-button';
+import { PrizmFocusableModule } from '../../../directives/focusable/focusable.module';
+import { PrizmIconModule } from '../../icon/icon.module';
+>>>>>>> d1d815f8e (feat: calendars v3 colors #987)
 
 /**
  * @deprecated
  * use standalone
  * */
 @NgModule({
+<<<<<<< HEAD
   imports: [PrizmPrimitiveYearMonthPaginationComponent],
+=======
+  imports: [
+    CommonModule,
+    PrizmFocusableModule,
+    PrizmPrimitiveSpinButtonModule,
+    PrizmIconModule,
+    PrizmMonthPipeModule,
+  ],
+  declarations: [PrizmPrimitiveYearMonthPaginationComponent],
+>>>>>>> d1d815f8e (feat: calendars v3 colors #987)
   exports: [PrizmPrimitiveYearMonthPaginationComponent],
 })
 export class PrizmPrimitiveYearMonthPaginationModule {}
