af3f6a131fee7c5ac91b8ce50972103daea98be4
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2023-11-07 23:37:41 +0300

    feat: calendars v3 colors #987

--- libs/components/src/lib/components/calendar-month/calendar-month.module.ts
+++ libs/components/src/lib/components/calendar-month/calendar-month.module.ts
@@ -3,13 +3,8 @@ import { NgModule } from '@angular/core';
 import { PrizmLetModule } from '@prizm-ui/helpers';
 import { PrizmFocusableModule } from '../../directives/focusable/focusable.module';
 import { PrizmHoveredModule } from '../../directives/hovered/hovered.module';
-import { PrizmPressedModule } from '../../directives/pressed/pressed.module';
-import { PrizmPrimitiveSpinButtonModule } from '../internal/primitive-spin-button/primitive-spin-button.module';
-import { PrizmPrimitiveYearPickerModule } from '../internal/primitive-year-picker/primitive-year-picker.module';
-import { PrizmLinkModule } from '../link/link.module';
-import { PrizmScrollbarModule } from '../scrollbar/scrollbar.module';
 import { PrizmCalendarMonthComponent } from './calendar-month.component';
-import { PrizmStopPropagationModule } from '../../directives/stop-propagation/stop-propagation.module';
+
 
 /**
  * @deprecated
