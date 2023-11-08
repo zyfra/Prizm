755c8c25495f8a361db8fc023e19b4a8652dec98
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-08 16:03:18 +0300

    feat(components/tabs): to standalone

--- libs/components/src/lib/components/tabs/tabs.module.ts
+++ libs/components/src/lib/components/tabs/tabs.module.ts
@@ -1,31 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PrizmIconModule } from '../icon';
 import { PrizmTabComponent } from './components/tab.component';
 import { PrizmTabsComponent } from './tabs.component';
-import { PrizmDropdownHostModule } from '../dropdowns/dropdown-host';
-import { PrizmButtonModule } from '../button';
-import { PolymorphModule, PrizmDropdownControllerModule, PrizmLifecycleModule } from '../../directives';
-import { PrizmDataListModule } from '../data-list';
 import { PrizmTabMenuItemDirective } from './tab-menu-item.directive';
-import { PrizmCallFuncModule, PrizmLetModule } from '@prizm-ui/helpers';
-import { PrizmCounterModule } from '../counter';
 
+/**
+ * @deprecated
+ * use standalone
+ * */
 @NgModule({
-  declarations: [PrizmTabsComponent, PrizmTabComponent, PrizmTabMenuItemDirective],
-  imports: [
-    CommonModule,
-    PrizmIconModule,
-    PolymorphModule,
-    PrizmLifecycleModule,
-    PrizmDropdownHostModule,
-    PrizmCallFuncModule,
-    PrizmLetModule,
-    PrizmButtonModule,
-    PrizmDropdownControllerModule,
-    PrizmDataListModule,
-    PrizmCounterModule,
-  ],
+  imports: [PrizmTabsComponent, PrizmTabComponent, PrizmTabMenuItemDirective],
   exports: [PrizmTabsComponent, PrizmTabComponent],
 })
 export class PrizmTabsModule {}
