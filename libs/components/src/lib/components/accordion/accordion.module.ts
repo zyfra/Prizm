78719dff9e89c48a3ca933327a97a88397c6783c
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2023-11-10 14:53:52 +0300

    fix: merge conflict fix

--- libs/components/src/lib/components/accordion/accordion.module.ts
+++ libs/components/src/lib/components/accordion/accordion.module.ts
@@ -3,11 +3,6 @@ import { PrizmAccordionComponent } from './accordion.component';
 import { PrizmAccordionContentDirective } from './directives/accordion-content.directive';
 import { AccordionToolsDirective } from './directives/accordion-tools.directive';
 import { PrizmAccordionItemComponent } from './components/accordion-item/accordion-item.component';
-<<<<<<< HEAD
-=======
-import { PolymorphModule } from '../../directives/polymorph';
-import { PrizmButtonModule } from '../button';
->>>>>>> 2c00b47f2 (feat: accordion and breadcrumbs v3 colors #929)
 
 /**
  * @deprecated
@@ -20,10 +15,6 @@ import { PrizmButtonModule } from '../button';
     AccordionToolsDirective,
     PrizmAccordionItemComponent,
   ],
-<<<<<<< HEAD
-=======
-  imports: [CommonModule, PrizmIconModule, PolymorphModule, PrizmButtonModule],
->>>>>>> 2c00b47f2 (feat: accordion and breadcrumbs v3 colors #929)
   exports: [
     PrizmAccordionComponent,
     PrizmAccordionContentDirective,
