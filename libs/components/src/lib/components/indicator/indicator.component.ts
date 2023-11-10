24865f82ee1130d6a3672aaa1fc3f5b6ceaaad6b
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-11-10 07:44:12 +0300

    feat(components/indicator): to standalone

--- libs/components/src/lib/components/indicator/indicator.component.ts
+++ libs/components/src/lib/components/indicator/indicator.component.ts
@@ -1,14 +1,17 @@
 import { Component, ChangeDetectionStrategy, Input, HostBinding } from '@angular/core';
 import { IndicatorStatus, IndicatorType } from './indicator.models';
 import { PrizmAbstractTestId } from '../../abstract/interactive';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'prizm-indicator',
   templateUrl: './indicator.component.html',
   styleUrls: ['./indicator.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [NgIf],
 })
-export class IndicatorComponent extends PrizmAbstractTestId {
+export class PrizmIndicatorComponent extends PrizmAbstractTestId {
   @Input() public type: IndicatorType = 'simple';
   @Input() public status: IndicatorStatus = 'info';
 
