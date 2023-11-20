6d2c9082881c03613996596c6efd5d598975bebf
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2023-11-20 14:05:53 +0300

    feat: textarea v3 colors update #1064

--- libs/components/src/lib/components/input/input-month-range/input-month-range.module.ts
+++ libs/components/src/lib/components/input/input-month-range/input-month-range.module.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { PrizmCalendarMonthModule } from '../../calendar-month';
+import { PrizmCalendarMonthComponent } from '../../calendar-month';
 import { PrizmMapperPipeModule } from '../../table/pipes/mapper/mapper.module';
 import { PrizmInputMonthRangeComponent } from './input-month-range.component';
 import { PrizmInputMonthRangeDirective } from './input-month-range.directive';
 import { FormsModule } from '@angular/forms';
-import { PrizmDropdownHostModule } from '../../dropdowns/dropdown-host';
+import { PrizmDropdownHostComponent } from '../../dropdowns/dropdown-host';
 import { PrizmPreventDefaultModule } from '../../../directives/prevent-default';
 import { PrizmInputTextModule } from '../input-text/input-text.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    PrizmCalendarMonthModule,
-    PrizmDropdownHostModule,
+    PrizmCalendarMonthComponent,
+    PrizmDropdownHostComponent,
     PrizmPreventDefaultModule,
     PrizmMapperPipeModule,
     PrizmInputTextModule,
