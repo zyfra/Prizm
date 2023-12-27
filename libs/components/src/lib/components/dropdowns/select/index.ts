0c25ef5b4d85aeaf40fed71f3fa5c474ed1ce159
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2023-12-27 12:33:22 +0300

    feat: removed componets

--- libs/components/src/lib/components/dropdowns/select/index.ts
+++ libs/components/src/lib/components/dropdowns/select/index.ts
@@ -1,5 +1,3 @@
-// export * from './select.component';
-// export * from './select.module';
 export * from './select.options';
 export * from './select.model';
 export * from './input-select.component';
