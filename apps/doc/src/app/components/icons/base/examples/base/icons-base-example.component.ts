30f7a36d9ba354d03ad03ec0165afae324f4790e
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-02-05 16:56:16 +0300

    feat(docs): output all icons

--- apps/doc/src/app/components/icons/base/examples/base/icons-base-example.component.ts
+++ apps/doc/src/app/components/icons/base/examples/base/icons-base-example.component.ts
@@ -18,7 +18,7 @@ import {
   imports: [PrizmIconsComponent, PrizmIcons16Component],
 })
 export class PrizmIconsBaseExampleComponent implements OnInit {
-  <!--    TODO: 4.0.0 rc.2 add different exports with same names -->
+  // TODO: 4.0.0 rc.2 add different exports with same names
   readonly icons16Registry = inject(PrizmIcons16Registry);
   readonly iconsBaseRegistry = inject(PrizmIconsRegistry);
 
