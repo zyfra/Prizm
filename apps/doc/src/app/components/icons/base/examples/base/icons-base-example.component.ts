93ffe911edbe1d39d319c21def1b2d5f43562d59
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-02-06 12:15:59 +0300

    feat: change names to full

--- apps/doc/src/app/components/icons/base/examples/base/icons-base-example.component.ts
+++ apps/doc/src/app/components/icons/base/examples/base/icons-base-example.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import {
   PrizmIconsFullComponent,
-  PrizmIcons16Registry,
+  PrizmIconsFullRegistry,
   prizmIconsArrowsCross,
   prizmIconsArrowUp,
   prizmIconsBag,
@@ -19,7 +19,7 @@ import {
 })
 export class PrizmIconsBaseExampleComponent implements OnInit {
   // TODO: 4.0.0 rc.2 add different exports with same names
-  readonly icons16Registry = inject(PrizmIcons16Registry);
+  readonly icons16Registry = inject(PrizmIconsFullRegistry);
   readonly iconsBaseRegistry = inject(PrizmIconsRegistry);
 
   public ngOnInit() {
