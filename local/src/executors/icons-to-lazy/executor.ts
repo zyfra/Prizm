8e7552a55a78f623d3b58ef497134140de2f3164
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-02-08 12:09:17 +0300

    feat(local): add icons name fixer

--- local/src/executors/icons-to-lazy/executor.ts
+++ local/src/executors/icons-to-lazy/executor.ts
@@ -24,6 +24,7 @@ export default async function runExecutor(options: IconsToLazyExecutorSchema) {
   const result: string[] = [];
   let skipped = 0;
 
+  const fixedNames: string[] = [];
   for (const file of files) {
     if (options.prefix && !file.toLowerCase().startsWith(options.prefix.toLowerCase())) {
       console.warn('SKIP FILE:', file);
@@ -31,14 +32,44 @@ export default async function runExecutor(options: IconsToLazyExecutorSchema) {
       continue;
     }
     const fileWithoutExt = file.split('.').slice(0, -1).join('.');
-    const content = fs.readFileSync(path.join(pathToFolder, file)).toString();
+    let content = fs.readFileSync(path.join(pathToFolder, file)).toString();
     const newPath = path.join(path.relative(destinationFolder, pathToFolder), fileWithoutExt);
+
+    /* fix names kebab between text + number (v1 > v-1) */
+    if (content) {
+      const matches = content.matchAll(/name: '[^']+([a-zA-Z]+-[0-9]+)'/g);
+      if (matches) for (const match of matches) {
+        const oldPart = match[1];
+        const newPath = match[1].replace(/-/g, '');
+
+        const changedPart = match[0].replace(
+          oldPart,
+          newPath
+        );
+
+        content = content.replaceAll(
+          match[0],
+          changedPart
+        );
+
+        fs.writeFileSync(
+          path.join(
+            pathToFolder,
+            file
+          ),
+          content,
+        );
+        fixedNames.push(`${oldPart}>${newPath}`);
+      }
+    }
+
     const exportName = /export const ([^:]+)/g.exec(content)?.[1];
     const iconName = /name: '([^']+)'/g.exec(content)?.[1];
 
     result.push(`'${iconName}': () => import('./${newPath}').then((r) => r['${exportName}'])`);
   }
 
+  console.warn('FIXED NAMES:', fixedNames);
   console.warn('IMPORTS SKIPPED:', skipped);
   console.warn('IMPORTS ADDED:', result.length);
 
