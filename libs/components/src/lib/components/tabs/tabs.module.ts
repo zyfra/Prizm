7c88cb111bcbc56b592ce3ec362f56ffecc1641d
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-02-22 13:37:22 +0300

    fix(component/tabs): error with double emitted idx on close tabls

--- libs/components/src/lib/components/tabs/tabs.module.ts
+++ libs/components/src/lib/components/tabs/tabs.module.ts
@@ -3,6 +3,9 @@ import { PrizmTabComponent } from './components/tab.component';
 import { PrizmTabsComponent } from './tabs.component';
 import { PrizmTabMenuItemDirective } from './tab-menu-item.directive';
 
+/**
+ * use standalone
+ * */
 @NgModule({
   imports: [PrizmTabsComponent, PrizmTabComponent, PrizmTabMenuItemDirective],
   exports: [PrizmTabsComponent, PrizmTabComponent],
