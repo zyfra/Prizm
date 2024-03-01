b61de5fdea1098b264a3d2d876d2a6eecca2f159
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2024-03-01 15:30:12 +0300

    fix: input password icon update to new pack

--- libs/components/src/lib/components/input/input-password/input-password-auxiliary-control.component.ts
+++ libs/components/src/lib/components/input/input-password/input-password-auxiliary-control.component.ts
@@ -49,7 +49,7 @@ export class PrizmInputPasswordDefaultControlComponent {
   ) {}
 
   get icon(): string {
-    return this.inputPassword.passwordHidden ? 'sort-eye' : 'sort-eye-off-2';
+    return this.inputPassword.passwordHidden ? 'eye' : 'eye-closed';
   }
 
   public toggle(): void {
