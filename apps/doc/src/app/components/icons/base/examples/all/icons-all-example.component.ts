76c01485e381c8c26fea84fb48c9d03420376b03
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-03-25 09:21:36 +0300

    feat(docs/icons): added new page with all icons register example

---
+++ apps/doc/src/app/components/icons/base/examples/all/icons-all-example.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { PrizmIconsComponent, PrizmIconsFullComponent } from '@prizm-ui/icons';
+import { PrizmIconsFullRegistry, PrizmIconsRegistry } from '@prizm-ui/icons/core';
+
+//  You must all icons from common set
+import * as allBaseIcons from '@prizm-ui/icons/base/source';
+import * as fullBaseIcons from '@prizm-ui/icons/full/source';
+import { PrizmIfLanguageDirective } from '@prizm-ui/i18n';
+
+@Component({
+  selector: 'prizm-icons-all-example',
+  templateUrl: './icons-all-example.component.html',
+  styleUrls: ['./icons-all-example.component.less'],
+  standalone: true,
+  imports: [PrizmIconsComponent, PrizmIconsFullComponent, PrizmIfLanguageDirective],
+})
+export class PrizmIconsAllExampleComponent implements OnInit {
+  readonly iconsFullRegistry = inject(PrizmIconsFullRegistry);
+  readonly iconsBaseRegistry = inject(PrizmIconsRegistry);
+
+  public ngOnInit() {
+    // here we use Object.values to get all icons from allBaseIcons and fullBaseIcons
+    this.iconsBaseRegistry.registerIcons(Object.values(allBaseIcons));
+    this.iconsFullRegistry.registerIcons(Object.values(fullBaseIcons));
+  }
+}
