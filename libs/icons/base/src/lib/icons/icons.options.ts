64bd50e247191968635c75bad71566f2fcce7b24
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-03-25 10:23:48 +0300

    feat: add preview label

--- libs/icons/base/src/lib/icons/icons.options.ts
+++ libs/icons/base/src/lib/icons/icons.options.ts
@@ -2,14 +2,23 @@ import { InjectionToken } from '@angular/core';
 import { prizmIconsGetNameByOld } from '../replace';
 
 type PrizmIconsNameTransformer = (name: string) => string | null;
+/**
+ * @developer-preview
+ * */
 export const PRIZM_ICONS_NAME_TRANSFORMER = new InjectionToken<PrizmIconsNameTransformer>(
   'icon names transformer'
 );
 
+/**
+ * @developer-preview
+ * */
 export function prizmIconsProvideOldNameTransformer() {
   return prizmIconsProvideTransformer(name => prizmIconsGetNameByOld(name) ?? name);
 }
 
+/**
+ * @developer-preview
+ * */
 export function prizmIconsProvideTransformer(transformer: PrizmIconsNameTransformer) {
   return {
     provide: PRIZM_ICONS_NAME_TRANSFORMER,
