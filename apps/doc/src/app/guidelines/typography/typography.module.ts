25f1e387101ef73ce6c35cd1384a2c1d1c1fbc81
Alexandr Fedorov <alexandr.fedorov@idp.zyfra.com>
2024-04-04 15:04:42 +0400

    fix: typography page changed #1497 #1498

--- apps/doc/src/app/guidelines/typography/typography.module.ts
+++ apps/doc/src/app/guidelines/typography/typography.module.ts
@@ -5,6 +5,7 @@ import { Frame4Component } from './components/frame4/frame4.component';
 import { Frame9Component } from './components/frame9/frame9.component';
 import { TypographyComponent } from './typography.component';
 import { PrizmAddonDocModule } from '@prizm-ui/doc';
+import { PrizmTableModule } from '@prizm-ui/components';
 
 const routes: Routes = [
   {
@@ -15,7 +16,7 @@ const routes: Routes = [
 
 @NgModule({
   bootstrap: [],
-  imports: [CommonModule, PrizmAddonDocModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, PrizmAddonDocModule, RouterModule.forChild(routes), PrizmTableModule],
   declarations: [TypographyComponent, Frame4Component, Frame9Component],
   exports: [RouterModule],
 })
