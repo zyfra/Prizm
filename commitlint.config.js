669cba9c2055b5f9bb72290231a23e6c6e1e4d22
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-04-22 16:54:55 +0300

    feat: added checker commit messages for #1622

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,20 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'references-empty': [2, 'never'], // Это правило требует наличия ссылок на issues в коммитах
+    'custom-rule-issue-number': [2, 'always'], // Настройка кастомного правила
+  },
+  plugins: [
+    {
+      rules: {
+        'custom-rule-issue-number': ({ header }) => {
+          const pattern = /#\d+/; // Пример паттерна, который ищет # за которым следуют цифры
+          return [
+            pattern.test(header),
+            'commit message must contain an issue number like #123 or #000 if internal',
+          ];
+        },
+      },
+    },
+  ],
+};
