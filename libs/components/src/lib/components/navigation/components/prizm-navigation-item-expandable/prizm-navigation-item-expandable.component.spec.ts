8a1544cb7316cdf5e5409edbbfe9b8fc5e825a1d
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-04-22 16:25:10 +0300

    fix: test with error ResizeObserver #1631

--- libs/components/src/lib/components/navigation/components/prizm-navigation-item-expandable/prizm-navigation-item-expandable.component.spec.ts
+++ libs/components/src/lib/components/navigation/components/prizm-navigation-item-expandable/prizm-navigation-item-expandable.component.spec.ts
@@ -6,6 +6,13 @@ import { ActiveNavigationItemService } from '../../services/active-navigation-it
 
 import { PrizmNavigationItemExpandableComponent } from './prizm-navigation-item-expandable.component';
 
+// for fix ResizeObserver is not defined
+global.ResizeObserver = class {
+  public observe() {}
+  public unobserve() {}
+  public disconnect() {}
+};
+
 describe('PrizmNavigationItemExpandableComponent', () => {
   let component: PrizmNavigationItemExpandableComponent;
   let fixture: ComponentFixture<PrizmNavigationItemExpandableComponent>;
