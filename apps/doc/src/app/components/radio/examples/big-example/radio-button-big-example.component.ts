78ae681cbd06e3dee5b2c43778add797c4a27340
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-05-16 11:13:46 +0300

    fix(doc): update examples for stackblitz #1595

--- apps/doc/src/app/components/radio/examples/big-example/radio-button-big-example.component.ts
+++ apps/doc/src/app/components/radio/examples/big-example/radio-button-big-example.component.ts
@@ -2,13 +2,13 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 
 @Component({
-  selector: 'prizm-radio-button-basic-example',
-  templateUrl: './radio-button-basic-example.component.html',
-  styleUrls: ['./radio-button-basic-example.component.less'],
+  selector: 'prizm-radio-button-big-example',
+  templateUrl: './radio-button-big-example.component.html',
+  styleUrls: ['./radio-button-big-example.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RadioButtonBasicExampleComponent {
-  public size: 'm' | 'l' | 's' = 's';
+export class RadioButtonBigExampleComponent {
+  @Input() public size: 'm' | 'l' | 's' = 'l';
   public data: string[] = ['First property', 'Second property', 'Third property'];
   public form = new UntypedFormControl({ value: this.data[1], disabled: false });
 }
--- apps/doc/src/app/components/radio/radio-button-example.module.ts
+++ apps/doc/src/app/components/radio/radio-button-example.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { prizmDocGenerateRoutes, PrizmAddonDocModule } from '@prizm-ui/doc';
+import { PrizmAddonDocModule, prizmDocGenerateRoutes } from '@prizm-ui/doc';
 import { RouterModule } from '@angular/router';
 import { RadioButtonBasicExampleComponent } from './examples/radio-button-basic-example/radio-button-basic-example.component';
 import { ExamplesRadioButtonComponent } from './radio-button-example.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PrizmRadioButtonModule } from '@prizm-ui/components';
+import { RadioButtonBigExampleComponent } from './examples/big-example/radio-button-big-example.component';
+import { RadioButtonSmallExampleComponent } from './examples/small-example/radio-button-small-example.component';
 
 @NgModule({
   imports: [
@@ -15,7 +17,12 @@ import { PrizmRadioButtonModule } from '@prizm-ui/components';
     RouterModule.forChild(prizmDocGenerateRoutes(ExamplesRadioButtonComponent)),
     ReactiveFormsModule,
   ],
-  declarations: [ExamplesRadioButtonComponent, RadioButtonBasicExampleComponent],
+  declarations: [
+    RadioButtonBigExampleComponent,
+    RadioButtonSmallExampleComponent,
+    ExamplesRadioButtonComponent,
+    RadioButtonBasicExampleComponent,
+  ],
   exports: [ExamplesRadioButtonComponent],
 })
 export class RadioButtonExampleModule {}
