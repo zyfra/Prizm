482d2813cbb384b3dcc67ff06e97e8cc2bbc8a46
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-06-13 13:46:11 +0300

    feat(doc): add v4 to doc

--- apps/doc/src/app/version-manager/versions.constants.ts
+++ apps/doc/src/app/version-manager/versions.constants.ts
@@ -12,20 +12,21 @@ export interface PrizmVersionMeta {
 
 export const PRIZM_VERSIONS_META: readonly PrizmVersionMeta[] = [
   {
-    label: '3.14.0 (ng16)',
-    version: '3.14.0',
+    label: '4.3.2 (ng17)',
+    version: '4.3.2',
     stackblitz: 'https://stackblitz.com/edit/prizm-v4-demo',
-    link: getDocSite.bind(null, 'http://doc.prizm.site', 'http://prizm.site'),
-    otherLinks: [new URL('https://prizm-v3.web.app')],
+    link: getDocSite.bind(null, 'https://doc.prizm.zyfra.com', 'http://prizm.site'),
+
+    otherLinks: [new URL('https://prizm-v4.web.app')],
     cb: (hostName: string, current: PrizmVersionMeta) => {
       return hostName.startsWith('prizm-v4--');
     },
   },
   {
-    label: '3.13.2 (ng16)',
-    version: '3.13.2',
+    label: '3.14.0 (ng16)',
+    version: '3.14.0',
     stackblitz: 'https://stackblitz.com/edit/prizm-v3-demo',
-    link: getDocSite.bind(null, 'http://3.13.2.doc.prizm.site', 'https://prizm-v3.web.app'),
+    link: getDocSite.bind(null, 'http://3.14.0.doc.prizm.site', 'https://prizm-v3.web.app'),
     otherLinks: [new URL('https://prizm-v3.web.app')],
     cb: (hostName: string, current: PrizmVersionMeta) => {
       return hostName.startsWith('prizm-v3--');
