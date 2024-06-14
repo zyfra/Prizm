07d9708f96b83bf36be4e7427a51463c6463b884
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2024-06-14 09:04:33 +0300

    fix(components/file-upload): file-size pipe logic update #000

--- libs/components/src/lib/components/file-upload/pipes/file-size.pipe.ts
+++ libs/components/src/lib/components/file-upload/pipes/file-size.pipe.ts
@@ -11,12 +11,12 @@ export class PrizmFileSizePipe implements PipeTransform {
         value: size,
         key: 'file_size__byte',
       };
-    } else if (size > 1024 && size < 1048576) {
+    } else if (size >= 1024 && size < 1048576) {
       return {
         value: (size / 1024).toFixed(1),
         key: 'file_size__kb',
       };
-    } else if (size > 1048576) {
+    } else if (size >= 1048576) {
       return {
         value: (size / 1048576).toFixed(1),
         key: 'file_size__mb',
