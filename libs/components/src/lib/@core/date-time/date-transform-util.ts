eb8dd78963c897b9437269e4a5ed0231d9ee58a4
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2024-07-03 16:34:19 +0300

    feat(components/input-date): add UTC and ISO strings support for min&max #1573

--- libs/components/src/lib/@core/date-time/date-transform-util.ts
+++ libs/components/src/lib/@core/date-time/date-transform-util.ts
@@ -2,8 +2,14 @@ import { PrizmDay } from './day';
 import { PrizmTime } from './time';
 
 export function transformDateIfNeeded(
-  value: PrizmDay | [PrizmDay, PrizmTime] | Date
+  value: PrizmDay | [PrizmDay, PrizmTime] | Date | string
 ): PrizmDay | [PrizmDay, PrizmTime] {
+  if (typeof value === 'string') {
+    value = new Date(value);
+
+    // TODO check id date is valid
+  }
+
   if (value instanceof Date) {
     return [PrizmDay.fromLocalNativeDate(value), PrizmTime.fromLocalNativeDate(value)];
   } else {
