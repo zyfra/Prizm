79de52d9fd62eed0d7c0d9624dd1c1281265dce0
darina.reynish@idp.zyfra.com <darina.reynish@idp.zyfra.com>
2024-07-17 12:49:59 +0300

    feat(chore): up version to 2.16.0 #000

--- apps/doc/src/app/version-manager/versions.constants.ts
+++ apps/doc/src/app/version-manager/versions.constants.ts
@@ -33,8 +33,8 @@ export const PRIZM_VERSIONS_META: readonly PrizmVersionMeta[] = [
     },
   },
   {
-    label: '2.15.1 (ng15)',
-    version: '2.15.1',
+    label: '2.16.0 (ng15)',
+    version: '2.16.0',
     stackblitz: 'https://stackblitz.com/edit/prizm-v2-demo',
     link: getDocSite.bind(null, 'http://2.14.2.doc.prizm.site', 'https://prizm-v2.web.app'),
     otherLinks: [],
