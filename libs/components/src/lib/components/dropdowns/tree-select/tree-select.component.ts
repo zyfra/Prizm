7bc705b641c4829a00ffa95053957a94c59be457
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-07-25 15:09:11 +0300

    feat(components/tree-select): stage 1 #000

---
+++ libs/components/src/lib/components/dropdowns/tree-select/tree-select.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'prizm-input-tree-select',
+  templateUrl: './tree-select.component.html',
+  styleUrls: ['./tree-select.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [],
+})
+export class PrizmInputTreeSelectComponent {}
