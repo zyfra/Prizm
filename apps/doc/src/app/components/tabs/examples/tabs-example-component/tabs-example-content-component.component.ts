a76ac8133f5561fd2038dfbcb28c1be9daef14f8
ickisIckis <darinafjordur@gmail.com>
2024-08-07 14:09:24 +0300

    fix(components/tabs): template usage for tabs listing #1859

--- apps/doc/src/app/components/tabs/examples/tabs-example-component/tabs-example-content-component.component.ts
+++ apps/doc/src/app/components/tabs/examples/tabs-example-component/tabs-example-content-component.component.ts
@@ -1,9 +1,9 @@
 import { Component, ChangeDetectionStrategy, Inject } from '@angular/core';
-import { POLYMORPH_CONTEXT, PrizmTabContext, PrizmTabItem } from '@prizm-ui/components';
+import { POLYMORPH_CONTEXT, PrizmTabContext } from '@prizm-ui/components';
 
 @Component({
   selector: 'prizm-tabs-example-component-content',
-  template: `{{ context.idx + 1 }}`,
+  template: `{{ context.idx + 1 }} element`,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TabsExampleComponentContentComponent {
