d19b3c0c94bb975aa99629049ffb52fe1ba5acc3
ickisIckis <darinafjordur@gmail.com>
2024-08-08 12:59:32 +0300

    fix(components/multiselect): usage with transformers should cover case with selectAll option #1919

--- libs/components/src/lib/components/dropdowns/multi-select/input-multi-select.component.ts
+++ libs/components/src/lib/components/dropdowns/multi-select/input-multi-select.component.ts
@@ -369,7 +369,7 @@ export class PrizmInputMultiSelectComponent<T> extends PrizmInputNgControl<T[]> 
     let values: T[];
     this.markAsTouched();
     if (this.isSelectAllItem(item)) {
-      values = newItemState ? [...this.items] : [];
+      values = newItemState ? this.items.map(item => this.transformer(item)) : [];
     } else {
       const selectedValue = this.transformer(item.obj);
       values = newItemState
