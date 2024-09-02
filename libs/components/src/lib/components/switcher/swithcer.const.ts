45d3ad9cfad0bf380dcd38ac5d72726c7316335f
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-09-02 09:50:51 +0300

    feat(components/switcher): added items #000

--- libs/components/src/lib/components/switcher/swithcer.const.ts
+++ libs/components/src/lib/components/switcher/swithcer.const.ts
@@ -1,1 +1,7 @@
+import { InjectionToken, ViewContainerRef } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 export const INITIAL_SWITHCER_INDEX = 0;
+export const SWITCHER_VIEW_CONTAINER = new InjectionToken<BehaviorSubject<ViewContainerRef>>(
+  'view ref for switcher'
+);
