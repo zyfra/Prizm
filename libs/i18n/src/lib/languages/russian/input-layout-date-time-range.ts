031c1176a32dcfa9a3bae7f68c294b0af6e82b80
ickisIckis <darinafjordur@gmail.com>
2024-10-16 15:23:56 +0300

    feat(components/input-layout-date-time-range): add localization for time label #2128

---
+++ libs/i18n/src/lib/languages/russian/input-layout-date-time-range.ts
@@ -0,0 +1,7 @@
+import { PrizmLanguageInputLayoutDateTimeRange } from '../../interfaces';
+
+export const PRIZM_RUSSIAN_INPUT_LAYOUT_DATE_TIME_RANGE: PrizmLanguageInputLayoutDateTimeRange = {
+  inputLayoutDateTimeRange: {
+    time: 'Время',
+  },
+};
