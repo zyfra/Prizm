44fa8a15547add1cb7923b716eabc1c59a57c4ff
ickisIckis <darinafjordur@gmail.com>
2024-11-02 15:18:29 +0300

    feat(doc/switcher): add overflow example #000

--- apps/doc/src/app/components/switcher/examples/switcher-overflow-example/switcher-overflow-example.component.ts
+++ apps/doc/src/app/components/switcher/examples/switcher-overflow-example/switcher-overflow-example.component.ts
@@ -1,6 +1,7 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { PrizmSwitcherItem } from '@prizm-ui/components';
 import { FormControl } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'prizm-switcher-overflow-example',
@@ -12,23 +13,32 @@ export class SwitcherOverflowExampleComponent {
   public readonly switchers: PrizmSwitcherItem[] = [
     {
       title: 'Таблицы',
-      disabled: true,
     },
     {
       title: 'Графики',
     },
     {
       title: 'Мнемосхемы',
-      appearanceType: 'outline',
     },
     {
       title: 'Дашборды',
-      appearance: 'primary',
     },
   ];
-  public readonly control = new FormControl(10);
+  public readonly switchers2: PrizmSwitcherItem[] = [
+    {
+      title: 'Таблицы',
+    },
+    {
+      title: 'Графики',
+    },
+  ];
+  public readonly switchers$ = new BehaviorSubject<PrizmSwitcherItem[]>(this.switchers);
+  public readonly control = new FormControl(1);
 
   public updateValue() {
     this.control.setValue(3);
+  }
+  public changeItems() {
+    this.switchers$.next(this.switchers$.value === this.switchers ? this.switchers2 : this.switchers);
   }
 }
