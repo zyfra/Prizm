3c224ae210d47103ddf5117d9fae761be51cc112
ickisIckis <darinafjordur@gmail.com>
2024-11-06 16:00:07 +0300

    feat(doc/navigation): remove extra files from demo #1365

--- apps/doc/src/app/components/navigation-menu/navigation-menu-example.component.ts
+++ apps/doc/src/app/components/navigation-menu/navigation-menu-example.component.ts
@@ -56,16 +56,9 @@ export class NavigationMenuExampleComponent {
     'item-groups.service.ts': import(
       './examples/navigation-menu-groups-example/example-data/item-groups.service.ts?raw'
     ),
-    'item-groups.constants.ts': import(
-      './examples/navigation-menu-groups-example/example-data/item-groups.constants.ts?raw'
-    ),
     'expanded-items.service.ts': import(
       './examples/navigation-menu-groups-example/example-data/expanded-items.service.ts?raw'
     ),
-    'interfaces.ts': import('./examples/navigation-menu-groups-example/example-data/interfaces.ts?raw'),
-    'hint-button.ts': import('./examples/hint-button/hint-button.component.html?raw'),
-    'hint-button.html': import('./examples/hint-button/hint-button.component.html?raw'),
-    'hint-button.less': import('./examples/hint-button/hint-button.component.less?raw'),
   };
 
   public readonly setupModule: RawLoaderContent = import('./examples/setup-module.md?raw');
