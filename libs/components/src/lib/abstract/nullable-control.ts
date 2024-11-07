21eea631a042e0c89e47189ceed5040f9c79f3cd
ickisIckis <darinafjordur@gmail.com>
2024-11-07 10:48:06 +0300

    feat(components/abstract): remove not used abstract classes #000

--- libs/components/src/lib/abstract/nullable-control.ts
+++
@@ -1,9 +0,0 @@
-import { Directive } from '@angular/core';
-import { AbstractPrizmControl } from './control';
-
-@Directive()
-export abstract class AbstractPrizmNullableControl<T> extends AbstractPrizmControl<T | null> {
-  protected getFallbackValue(): T | null {
-    return null;
-  }
-}
