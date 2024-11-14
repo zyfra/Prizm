ff3557a29d32e6b2e66525cd427025b2d5ffb04e
Zurab Developer <75216044+ZurabDev@users.noreply.github.com>
2024-11-14 11:47:28 +0300

    fix(addons/query-builder): in english showed only russian #402

--- libs/addons/query-builder/src/lib/i18n/service.ts
+++ libs/addons/query-builder/src/lib/i18n/service.ts
@@ -9,7 +9,7 @@ import { PRIZM_ENGLISH_QUERY_BUILDER } from './languages/en';
 })
 export class PrizmI18nQueryBuilderService extends PrizmI18nCustomService<PrizmLanguageQueryBuilder> {
   override fn: PrizmI18nFn<PrizmLanguageQueryBuilder> = key => {
-    if (key === 'english') return PRIZM_RUSSIAN_QUERY_BUILDER;
-    return PRIZM_ENGLISH_QUERY_BUILDER;
+    if (key === 'english') return PRIZM_ENGLISH_QUERY_BUILDER;
+    return PRIZM_RUSSIAN_QUERY_BUILDER;
   };
 }
