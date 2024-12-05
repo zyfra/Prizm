b032849bc49a832c2faaf9a75b98f968529851b5
ickisIckis <darinafjordur@gmail.com>
2024-12-05 11:08:25 +0300

    feat(components/input-layout-inserted-directive): add annotation for directive #276

--- libs/components/src/lib/components/input/common/input-layout/input-layout-inserted.directive.ts
+++ libs/components/src/lib/components/input/common/input-layout/input-layout-inserted.directive.ts
@@ -1,5 +1,11 @@
 import { Directive, HostBinding } from '@angular/core';
 
+/**
+ *  The directive is used to manage classes for inserted to tables input layout
+ *  while the styles themselves are managed directly in the component.
+ *  extension for data grids is expected
+ */
+
 @Directive({
   selector: 'prizm-input-layout[prizmInputLayoutInserted]',
   standalone: true,
