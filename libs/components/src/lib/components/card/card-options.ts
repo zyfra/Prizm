b4f6e702c56dea5875ab81de78c272de9f751c68
ickisIckis <darinafjordur@gmail.com>
2024-12-10 10:01:05 +0300

    feat(components/card): add card options provider #1669

---
+++ libs/components/src/lib/components/card/card-options.ts
@@ -0,0 +1,14 @@
+import { InjectionToken } from '@angular/core';
+import { PrizmShadowType } from '../../directives/shadow';
+
+export interface PrizmCardOptions {
+  shadow: PrizmShadowType;
+}
+
+export const prizmCardDefaultOptions: PrizmCardOptions = {
+  shadow: 'mini-bottom',
+};
+
+export const PRIZM_CARD_OPTIONS = new InjectionToken<Partial<PrizmCardOptions>>('Prizm card options', {
+  factory: (): PrizmCardOptions => prizmCardDefaultOptions,
+});
--- libs/components/src/lib/components/card/card.component.ts
+++ libs/components/src/lib/components/card/card.component.ts
@@ -1,24 +1,28 @@
-import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
-import { prizmGetShadow, PrizmShadowDirective, PrizmShadowType } from '../../directives/shadow';
+import { ChangeDetectionStrategy, Component, HostBinding, Inject, Input } from '@angular/core';
+import { prizmGetShadow, PrizmShadowType } from '../../directives/shadow';
 import { PrizmShadowValue } from '../../directives/shadow/models';
 import { PrizmAbstractTestId } from '../../abstract/interactive';
 import { CommonModule } from '@angular/common';
+import { PRIZM_CARD_OPTIONS, PrizmCardOptions } from './card-options';
 
 @Component({
   selector: 'prizm-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.less'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [],
   standalone: true,
-  imports: [CommonModule, PrizmShadowDirective],
+  imports: [CommonModule],
 })
 export class PrizmCardComponent extends PrizmAbstractTestId {
-  @Input() shadow: PrizmShadowType = 'mini-bottom';
+  @Input() shadow: PrizmShadowType = this.options.shadow;
 
   @HostBinding('style.box-shadow')
   private get boxShadow(): PrizmShadowValue {
     return prizmGetShadow(this.shadow);
+  }
+
+  constructor(@Inject(PRIZM_CARD_OPTIONS) private readonly options: PrizmCardOptions) {
+    super();
   }
 
   override readonly testId_ = 'ui_card';
