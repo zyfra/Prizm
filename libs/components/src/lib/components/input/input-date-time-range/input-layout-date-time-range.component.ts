c68d5b7bb8b4f0b4d1d55071bd72a4aa21d3c758
ickisIckis <darinafjordur@gmail.com>
2024-12-17 15:25:25 +0300

    fix(components/input-layout-date-range): import merge error correction #000

--- libs/components/src/lib/components/input/input-date-time-range/input-layout-date-time-range.component.ts
+++ libs/components/src/lib/components/input/input-date-time-range/input-layout-date-time-range.component.ts
@@ -34,7 +34,7 @@ import { PRIZM_DATE_TEXTS, PRIZM_INPUT_LAYOUT_DATE_TIME_RANGE, PRIZM_TIME_TEXTS 
 import { PRIZM_DATE_TIME_RANGE_VALUE_TRANSFORMER } from '../../../tokens/date-inputs-value-transformers';
 import { PrizmControlValueTransformer } from '../../../types/control-value-transformer';
 import { prizmNullableSame } from '../../../util/common/nullable-same';
-import { filterTruthy, PrizmDestroyService, PrizmPluckPipe } from '@prizm-ui/helpers';
+import { filterTruthy, PrizmDestroyService, PrizmLetDirective, PrizmPluckPipe } from '@prizm-ui/helpers';
 import { PrizmInputControl } from '../common/base/input-control.class';
 import { PrizmInputNgControl } from '../common/base/input-ng-control.class';
 import { debounceTime, delay, distinctUntilChanged, map, share, takeUntil, tap } from 'rxjs/operators';
@@ -53,8 +53,7 @@ import { PrizmDateTimeMinMax } from './model';
 import { prizmI18nInitWithKeys } from '../../../services';
 import { CommonModule } from '@angular/common';
 import { PrizmMaskModule } from '../../../modules';
-import { PrizmLifecycleDirective, PrizmValueAccessorModule } from '../../../directives';
-import { PrizmDataListComponent } from '../../data-list';
+import { PrizmLifecycleDirective } from '../../../directives';
 import { PrizmInputLayoutTimeModule } from '../input-time';
 import { PrizmInputTextModule } from '../input-text';
 import { PrizmDropdownHostComponent } from '../../dropdowns/dropdown-host';
@@ -96,6 +95,7 @@ import { PrizmLanguageInputLayoutDateTimeRange } from '@prizm-ui/i18n';
     PrizmInputTextModule,
     PrizmDropdownHostComponent,
     PrizmCalendarRangeComponent,
+    PrizmLetDirective,
     FormsModule,
     PrizmTimeConstraintsPipe,
     PrizmPluckPipe,
