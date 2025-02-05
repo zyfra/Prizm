e1e23526529d54c1b410f11b622840ce3498f43f
ickisIckis <darinafjordur@gmail.com>
2025-02-05 17:21:42 +0300

    fix(doc/date-time-range): add unsub for example #000

--- apps/doc/src/app/components/input/input-layout-date-time-range/examples/native-date/input-native-date-time-range-base-example.component.ts
+++ apps/doc/src/app/components/input/input-layout-date-time-range/examples/native-date/input-native-date-time-range-base-example.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl } from '@angular/forms';
 import { prizmGetInputDateTimeRangeNativeTransformer } from '@prizm-ui/components';
+import { PrizmDestroyService } from '@prizm-ui/helpers';
+import { takeUntil } from 'rxjs';
 
 @Component({
   selector: 'prizm-input-layout-date-time-range-native-example',
@@ -18,8 +20,10 @@ import { prizmGetInputDateTimeRangeNativeTransformer } from '@prizm-ui/component
 export class PrizmInputNativeDateRangeBaseExampleComponent implements OnInit {
   readonly value = new UntypedFormControl([new Date(2018, 2, 10), new Date(2018, 3, 20)]);
 
+  constructor(private readonly destroy$: PrizmDestroyService) {}
+
   ngOnInit(): void {
-    this.value.valueChanges.subscribe(res => {
+    this.value.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(res => {
       console.log(res);
     });
   }
