881b447383d319e6e73672597d74f362aa178cc5
ickisIckis <darinafjordur@gmail.com>
2025-04-07 15:55:07 +0300

    feat(components/none-content): remove extra files #000:

--- libs/i18n/src/lib/languages/english/none-content.ts
+++
@@ -1,8 +0,0 @@
-import { PrizmLanguageNoneContent } from '../../interfaces';
-
-export const PRIZM_ENGLISH_NONE_CONTENT: PrizmLanguageNoneContent = {
-  noneContent: {
-    heading: 'No data to display',
-    description: 'To view information in this area, you need to change the display or filtering conditions',
-  },
-};
